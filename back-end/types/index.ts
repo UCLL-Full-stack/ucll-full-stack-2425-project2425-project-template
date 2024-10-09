25823080bd90e8958481f42b1272453999b06003
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-10-09 13:59:50 +0200

    Create IngredientCategory type for ingredient categories

---
+++ back-end/types/index.ts
@@ -0,0 +1,24 @@
+type IngredientCategory =
+    | 'Vegetables'
+    | 'Fruits'
+    | 'Meats'
+    | 'Seafood'
+    | 'Dairy'
+    | 'Grain'
+    | 'Legumes'
+    | 'Nuts & Seeds'
+    | 'Spices & Herbs'
+    | 'Sauces'
+    | 'Oils'
+    | 'Vinegar & Wine'
+    | 'Sweetener'
+    | 'Beverage'
+    | 'Baking'
+    | 'Frozen'
+    | 'Canned'
+    | 'Pasta'
+    | 'Bread'
+    | 'PreparedFood'
+    | 'Other';
+
+export { IngredientCategory };
