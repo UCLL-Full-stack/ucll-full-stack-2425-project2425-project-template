7f8c149d4d32f6ba8268a5389e59eb3b62447248
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-10-12 18:02:50 +0200

    fix profile test

--- back-end/test/model/profile.test.ts
+++ back-end/test/model/profile.test.ts
@@ -1,12 +1,20 @@
 import { Profile } from '../../model/profile';
+import { User } from '../../model/user';
 
 const firstName1 = 'Test';
 const lastName1 = 'User';
 const email1 = 'test.user@ucll.be';
-const profile: Profile = new Profile({ firstName: firstName1, lastName: lastName1, email: email1 });
+const user1 = new User({ username: 'test user', password: 'str!ngP@ss123' });
+const profile: Profile = new Profile({
+    firstName: firstName1,
+    lastName: lastName1,
+    email: email1,
+    user: user1,
+});
 
 test('given: valid values for profile, when: profile is created, then: profile is created with those values', () => {
     expect(profile.getFirstName()).toBe(firstName1);
     expect(profile.getLastName()).toBe(lastName1);
     expect(profile.getEmail()).toBe(email1);
+    expect(profile.getUser()).toBe(user1);
 });
