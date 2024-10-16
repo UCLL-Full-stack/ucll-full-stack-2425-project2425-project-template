8ac6213bad2ebacb8993e714576adfedb060631c
thomasgorisucll <79001636+thomasgorisucll@users.noreply.github.com>
2024-10-16 21:28:27 +0200

    Tag model and getter and setter also an Equals method

---
+++ back-end/model/tags.ts
@@ -0,0 +1,54 @@
+import { Recipe } from "./recipe";
+
+export class Tag {
+    private _tagId?: number;
+    private _name: string;
+    private _description: string;
+    private _recipes: Recipe[];
+
+    constructor(tag: { tagId?: number, name: string, description: string, recipes: Recipe[] }) {
+        this._tagId = tag.tagId;
+        this._name = tag.name;
+        this._description = tag.description;
+        this._recipes = tag.recipes
+    }
+
+    getTagId(): number | undefined {
+        return this._tagId;
+    }
+    // No setter of id need this database will set it. 
+
+    getName(): string {
+        return this._name;
+    }
+
+    setName(name: string): void {
+        this._name = name;
+    }
+
+    getDescription(): string {
+        return this._description;
+    }
+
+    setDescription(description: string): void {
+        this._description = description;
+    }
+
+    getRecipes(): Recipe[] {
+        return this._recipes;
+    }
+
+    setRecipes(recipes: Recipe[]): void {
+        this._recipes = recipes;
+    }
+
+
+    //I haven't included the Id because this will normally never match 
+    //I also haven't included the recipes because if the have the same name and description the are still the same
+    equals(tag: Tag): boolean {
+        return(
+            this._name === tag._name &&
+            this._description === tag._description 
+        )
+    }
+}
