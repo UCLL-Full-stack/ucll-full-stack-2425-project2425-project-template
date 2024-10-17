48d30dbdd4712880c2aaaedf893786a9d98e1884
MengMassart_UCLL <r0891147@ucll.be>
2024-10-17 11:17:49 +0200

    Tiny descriptions added

--- back-end/model/Match.ts
+++ back-end/model/Match.ts
@@ -6,19 +6,19 @@ export class Match {
     date : Date;
     hall : string;
     square : number;
-    players? : Array<Players>;
+    players? : Array<Players>; // Unusable for now
 
     constructor (match: {
         id : number,
         date : Date,
         hall : string,
         square : number;
-        players? : Array<Players>;
+        players? : Array<Players>; // Unusable for now
     }) {
         this.matchId = match.id;
         this.date = match.date;
         this.hall = match.hall;
         this.square = match.square;
-        this.players = match.players || new Array<Players>();
+        this.players = match.players || new Array<Players>(); // Unusable for now
     }
 }
--- back-end/model/Team.ts
+++ back-end/model/Team.ts
@@ -3,16 +3,16 @@ import { User } from "./User";
 
 export class Team {
     teamId : number;
-    members? : Array<Members>;
+    members? : Array<Members>; // Unusable for now
     coach : User;
 
     constructor (team: {
         id : number,
-        members? : Array<Members>,
+        members? : Array<Members>, // Unusable for now
         coach : User,
     }) {
         this.teamId = team.id;
-        this.members = team.members || new Array<Members>();
+        this.members = team.members || new Array<Members>(); // Unusable for now
         this.coach = team.coach;
     }
 }
