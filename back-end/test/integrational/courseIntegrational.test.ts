40db8aae01caad415eb74296920af1a0c29498dc
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-10-19 22:48:40 +0000

    4/ implemented tests

--- back-end/test/integrational/courseIntegrational.test.ts
+++ back-end/test/integrational/courseIntegrational.test.ts
@@ -28,4 +28,28 @@ describe("Course router", () => {
             expect(response.body).toEqual(courses.map(course => new CourseShortView(course)));
         });
     });
+
+    describe("get a course by id", () => {
+        let id : number = 1
+        it("should return 200 and the course with the given id", async () => {
+            const response = await request(app)
+                .get("/courses/" + id)
+                .set("content-type", "application/json");
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual(courses.find(course => course.id === id));
+        });
+    });
+
+    describe("throw an error: course with id does not exist", () => {
+        let id : number = 112123
+        it("should return 500", async () => {
+            const response = await request(app)
+                .get("/courses/" + id)
+                .set("content-type", "application/json");
+
+            expect(response.status).toBe(500);
+            expect(response.text).toContain("Course with id 112123 does not exist");
+        });
+    });
 });
--- back-end/test/service/courseService.test.ts
+++ back-end/test/service/courseService.test.ts
@@ -2,7 +2,8 @@ import CourseShortView from "../../types/courseShortView";
 import courseService from "../../service/course.service";
 import courseRepository from "../../repository/course.db";
 import dummyCourses from "../../data/courses";
-let mockDBFindAllCourses: jest.Mock;
+let mockDBFindAll: jest.Mock;
+let mockDBFindById: jest.Mock;
 
 
 beforeEach(() => {});
@@ -12,21 +13,39 @@ afterEach(() => {
 });
 
 test("given a list of courses, when getAll is called, then it should return the list of courses", () => {
-    mockDBFindAllCourses = jest.fn(() => dummyCourses);
-    courseRepository.findAll = mockDBFindAllCourses;
+    mockDBFindAll = jest.fn(() => dummyCourses);
+    courseRepository.findAll = mockDBFindAll;
 
     const result = courseService.getAll();
 
     expect(result).toEqual(dummyCourses);
-    expect(mockDBFindAllCourses).toHaveBeenCalledTimes(1);
+    expect(mockDBFindAll).toHaveBeenCalledTimes(1);
 });
 
 test("given a list of courses, when getAllShort is called, then it should return the list of courses in short view", () => {
-    mockDBFindAllCourses = jest.fn(() => dummyCourses);
-    courseRepository.findAll = mockDBFindAllCourses;
+    mockDBFindAll = jest.fn(() => dummyCourses);
+    courseRepository.findAll = mockDBFindAll;
 
     const result = courseService.getAllShort();
 
     expect(result).toEqual(dummyCourses.map(course => new CourseShortView(course)));
-    expect(mockDBFindAllCourses).toHaveBeenCalledTimes(1);
+    expect(mockDBFindAll).toHaveBeenCalledTimes(1);
+});
+
+test("given a list of courses, when getCourseById is called, then it should return the course with this id", () => {
+    mockDBFindById = jest.fn(() => dummyCourses[1]);
+    courseRepository.findById = mockDBFindById;
+
+    const result = courseService.getCourseById(1);
+
+    expect(result).toEqual(dummyCourses[1]);
+    expect(mockDBFindById).toHaveBeenCalledTimes(1);
+});
+
+test("given a list of courses and wrong ID, when getCourseById is called, then it should throw an exception", () => {
+    mockDBFindById = jest.fn(() => null);
+    courseRepository.findById = mockDBFindById;
+
+    expect(() => courseService.getCourseById(1)).toThrow(`Course with id 1 does not exist`);
+    expect(mockDBFindById).toHaveBeenCalledTimes(1);
 });
