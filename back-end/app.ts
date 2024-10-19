dbc6086d1f33986788e801af920b48892c3a7760
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-10-19 10:57:53 +0000

    2/ mapped url courses with the router in app.ts

--- back-end/app.ts
+++ back-end/app.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
+import { courseRouter } from './controller/course.routes';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
@@ -11,6 +12,20 @@ const port = process.env.APP_PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/courses", courseRouter);
+
+const swaggerOpts = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Courses API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./controller/*.routes.ts'],
+};
+const swaggerSpec = swaggerJSDoc(swaggerOpts);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/status', (req, res) => {
     res.json({ message: 'Back-end is running...' });
