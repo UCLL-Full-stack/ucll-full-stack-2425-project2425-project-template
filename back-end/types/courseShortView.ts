eace02932afa8b742ee27eb3aa1342316792f5b1
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-10-19 22:20:58 +0000

    3/ Added courseShortView to map the courses

---
+++ back-end/types/courseShortView.ts
@@ -0,0 +1,13 @@
+export default class Course {
+    public id: number;
+    public name: string;
+    public phase: number;
+    public credits: number;
+
+    constructor(course: { id: number; name: string; phase: number; credits: number}) {
+        this.id = course.id;
+        this.name = course.name;
+        this.phase = course.phase;
+        this.credits = course.credits;
+    }
+}
