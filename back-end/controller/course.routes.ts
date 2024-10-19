761a887a042bef22041b9caf6324d2163e362227
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-10-19 22:23:58 +0000

    3/ added swagger documentation

--- back-end/controller/course.routes.ts
+++ back-end/controller/course.routes.ts
@@ -1,36 +1,51 @@
 /**
  * @swagger
  *   components:
- *    securitySchemes:
- *     bearerAuth:
- *      type: http
- *      scheme: bearer
- *      bearerFormat: JWT
- *    schemas:
- *      Course:
- *        type: object
- *        properties:
- *          id:
- *            type: number
- *            format: int64
- *          name:
- *            type: string
- *            description: Course name.
- *          description:
- *            type: string
- *            description: Course description.
- *          phase:
- *            type: number
- *            description: Course phase.
- *          credits:
- *            type: number
- *            description: Course credits.
- *          lecturers: 
- *            type: array
- *            items:
- *              type: string
- *          isElective:
- *            type: boolean
+ *     securitySchemes:
+ *       bearerAuth:
+ *         type: http
+ *         scheme: bearer
+ *         bearerFormat: JWT
+ *     schemas:
+ *       Course:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: number
+ *             format: int64
+ *           name:
+ *             type: string
+ *             description: Course name.
+ *           description:
+ *             type: string
+ *             description: Course description.
+ *           phase:
+ *             type: number
+ *             description: Course phase.
+ *           credits:
+ *             type: number
+ *             description: Course credits.
+ *           lecturers: 
+ *             type: array
+ *             items:
+ *               type: string
+ *           isElective:
+ *             type: boolean
+ *       CourseShortView:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: number
+ *             format: int64
+ *           name:
+ *             type: string
+ *             description: Course name.
+ *           phase:
+ *             type: number
+ *             description: Course phase.
+ *           credits:
+ *             type: number
+ *             description: Course credits.
  */
 
 import express, { NextFunction, Request, Response } from 'express';
@@ -63,6 +78,22 @@ courseRouter.get("/" , async (req: Request, res: Response, next: NextFunction) =
 });
 
 
+/**
+ * @swagger
+ * /course:
+ *   get:
+ *     summary: Get all courses in short form
+ *     tags: [Course]
+ *     responses:
+ *       200:
+ *         description: All courses
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/CourseShortView'
+ */
 courseRouter.get("/short" , async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).json(courseService.getAllShort());
