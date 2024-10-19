a1d107640bac1932e5b36c96b72e098194e1fc87
Fnafke <elin.bachvarov@student.ucll.be>
2024-10-19 15:45:50 +0200

    User class added (no tests yet)

--- back-end/model/user.ts
+++ back-end/model/user.ts
@@ -1,13 +1,13 @@
 import { Role } from "../types";
 
 export class User {
-    private name: String;
-    private email: String;
-    private password: String;
+    private name: string;
+    private email: string;
+    private password: string;
     private role: Role;
 
 
-    constructor(user: {name: String, email: String, password: String, role: Role}) {
+    constructor(user: {name: string, email: string, password: string, role: Role}) {
         this.validate(user);
 
         this.name = user.name;
@@ -16,8 +16,20 @@ export class User {
         this.role = user.role;
     }
 
-    validate(user: {name: String, email: String, password: String, role: Role}) {
+    validate(user: {name: string, email: string, password: string, role: Role}) {
+       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
        
+       if (!user.name || user.name.trim().length < 1) {
+        throw new Error("Name must not empty.");
+       }
+       
+       if (!emailRegex.test(user.email)) {
+        throw new Error("Email is not valid format.");
+       }
+
+       if (user.password.length < 8) {
+        throw new Error("Password must be at least 8 characters long.")
+       }
     }
 
     getName(): String {
