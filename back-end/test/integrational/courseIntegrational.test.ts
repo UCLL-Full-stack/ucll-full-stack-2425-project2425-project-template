1feecc234305caeb6469d41f2172e3263d7ed295
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-10-20 15:24:34 +0000

    5/ implemented tests

--- back-end/test/integrational/courseIntegrational.test.ts
+++ back-end/test/integrational/courseIntegrational.test.ts
@@ -1,15 +1,14 @@
-const request = require("supertest");
-const app = require("../../app");
-import courses from "../../data/courses";
-import CourseShortView from "../../types/courseShortView";
+const request = require('supertest');
+const app = require('../../app');
+import courses from '../../data/courses';
+import CourseShortView from '../../types/courseShortView';
 
-describe("Course router", () => {
-
-    describe("get all courses", () => {
-        it("should return 200 and the courses", async () => {
+describe('Course router', () => {
+    describe('get all courses', () => {
+        it('should return 200 and the courses', async () => {
             const response = await request(app)
-                .get("/courses")
-                .set("content-type", "application/json");
+                .get('/courses')
+                .set('content-type', 'application/json');
 
             expect(response.status).toBe(200);
             expect(response.body.length).toBeGreaterThan(1);
@@ -17,39 +16,65 @@ describe("Course router", () => {
         });
     });
 
-    describe("get all courses short", () => {
-        it("should return 200 and the courses in short form", async () => {
+    describe('get all courses short', () => {
+        it('should return 200 and the courses in short form', async () => {
             const response = await request(app)
-                .get("/courses/short")
-                .set("content-type", "application/json");
+                .get('/courses/short')
+                .set('content-type', 'application/json');
 
             expect(response.status).toBe(200);
             expect(response.body.length).toBeGreaterThan(1);
-            expect(response.body).toEqual(courses.map(course => new CourseShortView(course)));
+            expect(response.body).toEqual(courses.map((course) => new CourseShortView(course)));
         });
     });
 
-    describe("get a course by id", () => {
-        let id : number = 1
-        it("should return 200 and the course with the given id", async () => {
+    describe('get a course by id', () => {
+        let id: number = 1;
+        it('should return 200 and the course with the given id', async () => {
             const response = await request(app)
-                .get("/courses/" + id)
-                .set("content-type", "application/json");
+                .get('/courses/' + id)
+                .set('content-type', 'application/json');
 
             expect(response.status).toBe(200);
-            expect(response.body).toEqual(courses.find(course => course.id === id));
+            expect(response.body).toEqual(courses.find((course) => course.id === id));
         });
     });
 
-    describe("throw an error: course with id does not exist", () => {
-        let id : number = 112123
-        it("should return 500", async () => {
+    describe('throw an error: course with id does not exist', () => {
+        let id: number = 112123;
+        it('should return 400', async () => {
             const response = await request(app)
-                .get("/courses/" + id)
+                .get('/courses/' + id)
+                .set('content-type', 'application/json');
+
+            expect(response.status).toBe(400);
+            expect(response.text).toContain('Course with id 112123 does not exist');
+        });
+    });
+
+    describe('delete courses by ids', () => {
+        it('should return 200 and a success message', async () => {
+            const ids = courses.slice(0, 3).map((course) => course.id);
+            const response = await request(app)
+                .delete('/courses/delete')
+                .send(ids)
+                .set('content-type', 'application/json');
+
+            expect(response.status).toBe(200);
+            expect(response.text).toEqual('Courses are successfully deleted');
+        });
+    });
+
+    describe("throw an error: one of the course ids does not exist", () => {
+        it("should return 500", async () => {
+            const ids = [999999];
+            const response = await request(app)
+                .delete("/courses/delete")
+                .send(ids)
                 .set("content-type", "application/json");
 
-            expect(response.status).toBe(500);
-            expect(response.text).toContain("Course with id 112123 does not exist");
+            expect(response.status).toBe(400);
+            expect(response.text).toContain("Course with id 999999 does not exist");
         });
     });
 });
--- back-end/test/service/courseService.test.ts
+++ back-end/test/service/courseService.test.ts
@@ -2,8 +2,10 @@ import CourseShortView from "../../types/courseShortView";
 import courseService from "../../service/course.service";
 import courseRepository from "../../repository/course.db";
 import dummyCourses from "../../data/courses";
+
 let mockDBFindAll: jest.Mock;
 let mockDBFindById: jest.Mock;
+let mockDBDeleteCourses: jest.Mock;
 
 
 beforeEach(() => {});
@@ -48,4 +50,36 @@ test("given a list of courses and wrong ID, when getCourseById is called, then i
 
     expect(() => courseService.getCourseById(1)).toThrow(`Course with id 1 does not exist`);
     expect(mockDBFindById).toHaveBeenCalledTimes(1);
+});
+
+test("given a list of courses and wrong ID, when getCourseById is called, then it should throw an exception", () => {
+    mockDBFindById = jest.fn(() => null);
+    courseRepository.findById = mockDBFindById;
+
+    expect(() => courseService.getCourseById(1)).toThrow(`Course with id 1 does not exist`);
+    expect(mockDBFindById).toHaveBeenCalledTimes(1);
+});
+
+test('given a list of course IDs, when deleteCourses is called, then it should delete the courses', () => {
+    mockDBFindById = jest.fn((id) => ({ id, name: `Course ${id}` }));
+    mockDBDeleteCourses = jest.fn();
+    courseRepository.findById = mockDBFindById;
+    courseRepository.deleteCourses = mockDBDeleteCourses;
+
+    const ids = [1, 2, 3];
+    const result = courseService.deleteCourses(ids);
+
+    expect(result).toBe("Courses are successfully deleted");
+    expect(mockDBFindById).toHaveBeenCalledTimes(ids.length);
+    expect(mockDBDeleteCourses).toHaveBeenCalledWith(ids);
+});
+
+test('given a list of course IDs with one non-existent ID, when deleteCourses is called, then it should throw an exception', () => {
+    mockDBFindById = jest.fn((id) => id === 2 ? null : { id, name: `Course ${id}` });
+    courseRepository.findById = mockDBFindById;
+
+    const ids = [1, 2, 3];
+
+    expect(() => courseService.deleteCourses(ids)).toThrow(`Course with id 2 does not exist`);
+    expect(mockDBFindById).toHaveBeenCalledTimes(2);
 });
