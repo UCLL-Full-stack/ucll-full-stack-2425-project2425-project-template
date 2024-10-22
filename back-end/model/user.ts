f301980e4a9e2ce168caac38b1dcc64ae983d7e5
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-10-22 06:53:37 +0200

    add relationship with recipe and schedule

--- back-end/model/user.ts
+++ back-end/model/user.ts
@@ -1,16 +1,29 @@
 import { Profile } from './profile';
+import { Recipe } from './recipe';
+import { Schedule } from './schedule';
 
 export class User {
     private id?: number;
     private username: string;
     private password: string;
     private profile?: Profile;
+    private recipes?: Recipe[];
+    private schedules?: Schedule[];
 
-    constructor(user: { id?: number; username: string; password: string; profile?: Profile }) {
+    constructor(user: {
+        id?: number;
+        username: string;
+        password: string;
+        profile?: Profile;
+        recipes?: Recipe[];
+        schedules?: Schedule[];
+    }) {
         this.id = user.id;
         this.username = user.username;
         this.password = user.password;
         this.profile = user.profile;
+        this.recipes = user.recipes || [];
+        this.schedules = user.schedules || [];
     }
 
     getId(): number | undefined {
@@ -27,6 +40,22 @@ export class User {
 
     getProfile(): Profile | undefined {
         return this.profile;
+    }
+
+    getRecipes(): Recipe[] | undefined {
+        return this.recipes;
+    }
+
+    getSchedules(): Schedule[] | undefined {
+        return this.schedules;
+    }
+
+    addRecipe(recipe: Recipe): void {
+        this.recipes?.push(recipe);
+    }
+
+    addSchedule(schedule: Schedule): void {
+        this.schedules?.push(schedule);
     }
 
     equals(user: User): boolean {
