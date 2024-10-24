b6b538abe39a7ac61a703f6834429cf7ab0998d9
Iniaz Steyaert <iniazsteyaert@MacBook-Air-van-Iniaz.local>
2024-10-24 16:24:29 +0200

    added all backend functionality for creating events

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,9 +1,10 @@
 import * as dotenv from 'dotenv';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import { eventRouter } from './controller/event.router';
 
 const app = express();
 dotenv.config();
@@ -12,10 +13,39 @@ const port = process.env.APP_PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use('/events', eventRouter);
+
 app.get('/status', (req, res) => {
     res.json({ message: 'Back-end is running...' });
 });
 
 app.listen(port || 3000, () => {
     console.log(`Back-end is running on port ${port}.`);
+});
+
+app.get('/status', (req, res) => {
+    res.json({ message: 'Courses API is running...' });
+});
+
+const swaggerOpts = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Courses API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./controller/*.routes.ts'],
+};
+const swaggerSpec = swaggerJSDoc(swaggerOpts);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ status: 'unauthorized', message: err.message });
+    } else if (err.name === 'CoursesError') {
+        res.status(400).json({ status: 'domain error', message: err.message });
+    } else {
+        res.status(400).json({ status: 'application error', message: err.message });
+    }
 });
