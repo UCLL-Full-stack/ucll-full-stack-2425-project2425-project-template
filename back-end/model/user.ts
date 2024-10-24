a896763b833ef9c523146d73eef0d80487155ba2
Amelie De Cre <amelie.decre@gmail.com>
2024-10-24 15:45:58 +0200

    username added to model

--- back-end/model/user.ts
+++ back-end/model/user.ts
@@ -1,16 +1,22 @@
 export class User {
     private id?: number;
+    private username: string;
     private email: string;
     private password: string;
 
-    constructor(user: {id?: number; email: string; password: string; }) {
+    constructor(user: {id?: number; username: string; email: string; password: string; }) {
         this.id = user.id;
+        this.username = user.username;
         this.email = user.email;
         this.password = user.password;
     }
 
     getId(): number | undefined {
         return this.id;
+    }
+
+    getUsername(): string {
+        return this.username;
     }
 
     getEmail(): string {
@@ -23,10 +29,14 @@ export class User {
 
     validate(user: {
         email: string;
+        username: string;
         password: string;
     }) {
         if (!user.email?.trim()) {
             throw new Error('Email is required');
+        }
+        if (!user.username?.trim()) {
+            throw new Error('Username is required');
         }
         if (!user.password?.trim()) {
             throw new Error('Password name is required');
@@ -35,6 +45,7 @@ export class User {
     equals(user: User): boolean {
         return (
            this.email === user.getEmail() &&
+           this.username == user.getUsername() &&
            this.password === user.getPassword() 
         );
     }
