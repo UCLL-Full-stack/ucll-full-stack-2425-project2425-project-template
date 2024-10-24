4945f33b3960052d158881d77235581eb1709047
HOANG TRUNG HIEU <trunghieu.hoang@student.ucll.be>
2024-10-24 18:25:20 +0200

    edit-style

--- front-end/components/header.tsx
+++ front-end/components/header.tsx
@@ -2,14 +2,26 @@ import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header className="p-3 mb-3 border-bottom bg-dark bg-gradient">
-      <a className="fs-2 d-flex justify-content-center mb-2 mb-lg-0 text-white-50 text-decoration-none">
+    <header className="p-3 mb-3 border-bottom bg-black">
+      <a className="fs-1 d-flex justify-content-center mb-2 mb-lg-0 text-white text-decoration-none">
         {' '}
         Eventora
       </a>
       <nav className="nav justify-content-center">
-        <Link href="/" className="nav-link px-4 fs-5 text-white">
+        <Link href="/" className="nav-link px-4 fs-5 text-white-50">
           Home
+        </Link>
+
+        <Link href="/" className="nav-link px-4 fs-5 text-white-50">
+          Upcoming events
+        </Link>
+
+        <Link href="/" className="nav-link px-4 fs-5 text-white-50">
+          My events
+        </Link>
+
+        <Link href="/" className="nav-link px-4 fs-5 text-white-50">
+          Create event
         </Link>
       </nav>
     </header>
