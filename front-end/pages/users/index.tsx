ed792d21853cf8d2a9422aa235099b7447abe948
Jorrit Antonio <jorrit.antonio@student.ucll.be>
2024-10-30 21:17:08 +0100

    Working getusers NO TESTS

---
+++ front-end/pages/users/index.tsx
@@ -0,0 +1,14 @@
+import Header from '@components/header'
+import Head from 'next/head'
+const Users: React.FC = () => {
+    return (
+        <>
+            <Head>
+                <title>Users</title>
+            </Head>
+            <Header/>
+        </>
+    )
+}
+
+export default Users
