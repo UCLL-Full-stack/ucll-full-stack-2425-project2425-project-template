236a072c431b613b2ae4bac3fd4a69ccc0d9fbab
Dhruv Agarwal <dhruv.agarwal@student.ucll.be>
2024-10-30 15:25:37 +0100

    1/ added positive test code for course

--- back-end/test/model/course.test.ts
+++ back-end/test/model/course.test.ts
@@ -1,16 +1,6 @@
 import { Course } from "../../model/course";
 
 
-const course = new Course({
-    id: 1,
-    name: 'Full-stack development',
-    description: 'Learn how to develop a full-stack app',
-    phase: 2,
-    credits: 6,
-    lecturers: ['Dr. Smith'],
-    isElective: true,
-    requiredPassedCourses: []
-});
 
 test('given: valid values for course, when: course is created, then: course is created with those values', () => {
     // when
