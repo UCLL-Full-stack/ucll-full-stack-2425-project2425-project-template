0b0acfb3f5eb5909743bfffc5428c294c6d7d998
HOANG TRUNG HIEU <trunghieu.hoang@student.ucll.be>
2024-10-30 13:30:59 +0100

    edit EventOverview

--- front-end/components/events/EventOverview.tsx
+++ front-end/components/events/EventOverview.tsx
@@ -7,6 +7,33 @@ type Props = {
 const EventOverview: React.FC<Prop> = ({events}: Props) => {
     return (
         <>
+            {events && events.length > 0 ? (
+                <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Event name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Location</th>
+                    <th scope="col">Capacity</th>
+                    <th scope="col">Category</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {events.map((event, index) => (
+                    <tr key={index} onClick={}>
+                      <td>{event.name}</td>
+                      <td>{event.description}</td>
+                      <td>{new Date(event.date).toLocaleDateString()}</td>
+                      <td>{event.location}</td>
+                      <td>{event.category}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p>There are no events yet.</p>
+            )}
         </>
     )
 };
---
+++ front-end/types/index.tsx
@@ -0,0 +1,8 @@
+export type EventInput = {
+    id?: number,
+    name: string,
+    description: string,
+    date: Date;
+    location: string,
+    category: string,
+};
