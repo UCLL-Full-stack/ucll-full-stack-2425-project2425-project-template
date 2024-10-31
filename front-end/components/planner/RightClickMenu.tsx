8d293e11ebda20889d7a5c6ffb3cfe2476fab0c3
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-10-31 15:40:58 +0100

    create component for the right click on a day cell from the calendar, install and import ContextMenu components from shadcn

---
+++ front-end/components/planner/RightClickMenu.tsx
@@ -0,0 +1,8 @@
+import { Plus, Trash, Copy, ClipboardPaste } from "lucide-react";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
