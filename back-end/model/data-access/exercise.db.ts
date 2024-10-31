9ea14ce4028f0ec81ebbb8f0de8f13a94a4ea6a8
AlexanderVanvinckenroyeUCLL <113980829+AlexanderVanvinckenroyeUCLL@users.noreply.github.com>
2024-10-31 21:15:56 +0100

    dummy data

--- back-end/model/data-access/exercise.db.ts
+++ back-end/model/data-access/exercise.db.ts
@@ -7,26 +7,63 @@ const defaultWorkoutExercise = new WorkoutExercise({
     exercise_id: 0,
     sets: 3,
     reps: 10,
-    rpe: '8',
-    rest_time: '01:00'
-});
-
+    rpe: "8",
+    rest_time: "01:00",
+  });
+  
 const exercises: Exercise[] = [
     new Exercise({
-        id: 1,
-        name: 'push-up',
-        description: 'A basic upper body strength exercise.',
-        video_link: 'https://youtu.be/IODxDxX7oi4?si=r9RqbT14IBF6aI5X',
-        workoutExercise: defaultWorkoutExercise
+      id: 1,
+      name: "push-up",
+      description: "A basic upper body strength exercise.",
+      video_link: "https://youtu.be/IODxDxX7oi4?si=r9RqbT14IBF6aI5X",
+      workoutExercise: defaultWorkoutExercise,
     }),
     new Exercise({
-        id: 2,
-        name: 'dips',
-        description: 'An exercise for the triceps and chest muscles.',
-        video_link: 'https://youtu.be/yN6Q1UI_xkE?si=DFFTgnjpAAIR-diV',
-        workoutExercise: defaultWorkoutExercise
-    })
-];
+      id: 2,
+      name: "dips",
+      description: "An exercise for the triceps and chest muscles.",
+      video_link: "https://youtu.be/yN6Q1UI_xkE?si=DFFTgnjpAAIR-diV",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 3,
+      name: "squat",
+      description: "A lower body exercise targeting the quads and glutes.",
+      video_link: "https://youtu.be/1oed-UmAxFs",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 4,
+      name: "lunges",
+      description: "A leg exercise focusing on the quads, hamstrings, and glutes.",
+      video_link: "https://youtu.be/QOVaHwm-Q6U",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 5,
+      name: "bench press",
+      description: "An upper body strength exercise for the chest and triceps.",
+      video_link: "https://youtu.be/rT7DgCr-3pg",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 6,
+      name: "deadlift",
+      description: "A full-body exercise focusing on the lower back and legs.",
+      video_link: "https://youtu.be/op9kVnSso6Q",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 7,
+      name: "pull-up",
+      description: "An upper body exercise for the back and biceps.",
+      video_link: "https://youtu.be/eGo4IYlbE5g",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+
+  ];
+  
 
 
 
@@ -38,4 +75,4 @@ const getExerciseById = (id: number): Exercise | undefined => {
     return exercises.find((exercise) => exercise.id === id);
 };
 
-export default {getAllExercises, getExerciseById}
+export default {getAllExercises, getExerciseById, exercises, defaultWorkoutExercise};
--- back-end/model/data-access/workout.db.ts
+++ back-end/model/data-access/workout.db.ts
@@ -1,6 +1,115 @@
+import { Exercise } from "../exercise";
 import { Workout } from "../workout";
+import { WorkoutExercise } from "../workoutexercise";
 
-const workouts: Workout[] = []
+const defaultWorkoutExercise = new WorkoutExercise({
+    workout_exercise_id: 0,
+    workout_id: 0,
+    exercise_id: 0,
+    sets: 3,
+    reps: 10,
+    rpe: "8",
+    rest_time: "01:00",
+  });
+  
+  const exercises: Exercise[] = [
+    new Exercise({
+      id: 1,
+      name: "Bench Press",
+      description: "This exercise focusses on the middle chest and triceps.",
+      video_link: "https://www.youtube.com/watch?v=rT7DgCr-3pg",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 2,
+      name: "Incline Smith Machine Press",
+      description: "This exercise focusses on the upper chest and triceps.",
+      video_link: "https://www.youtube.com/watch?v=8urE8Z8AMQ4",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 3,
+      name: "Chest Fly",
+      description: "Isolation exercise for the chest.",
+      video_link: "https://www.youtube.com/watch?v=FDay9wFe5uE",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 4,
+      name: "Pull up",
+      description: "This exercise focusses on the back and biceps.",
+      video_link: "https://www.youtube.com/watch?v=eGo4IYlbE5g",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 5,
+      name: "Chin up",
+      description: "This exercise focusses on the back and biceps.",
+      video_link: "https://www.youtube.com/watch?v=8mryJ3w2S78",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 6,
+      name: "Chest supported row",
+      description: "This exercise focusses on your back musles.",
+      video_link: "https://www.youtube.com/watch?v=0UBRfiO4zDs",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 7,
+      name: "Squat",
+      description: "This exercise focusses on the legs.",
+      video_link: "https://www.youtube.com/watch?v=m0GcZ24pK6k",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 8,
+      name: "Leg Extension",
+      description: "An isolation exercise for the quads.",
+      video_link: "https://www.youtube.com/watch?v=4ZDm5EbiFI8",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 9,
+      name: "Seated Leg Curl",
+      description: "An isolation exercise for the hamstrings.",
+      video_link: "https://www.youtube.com/watch?v=Orxowest56U",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+    new Exercise({
+      id: 10,
+      name: "Plank",
+      description: "This exercise focusses on the core.",
+      video_link: "https://www.youtube.com/watch?v=pvIjsG5Svck",
+      workoutExercise: defaultWorkoutExercise,
+    }),
+  ];
+
+const workouts: Workout[] = [
+    new Workout({
+      workout_id: 1,
+      user_id: 1,
+      name: "Chest Workout",
+      description: "Workout focussing on the chest muscles.",
+      exercises: [exercises[0], exercises[1], exercises[2]], 
+    }),
+    new Workout({
+      workout_id: 2,
+      user_id: 2,
+      name: "Back Workout",
+      description: "Workout focussing on the back muscles.",
+      exercises: [exercises[3], exercises[4], exercises[5]], 
+    }),
+    new Workout({
+      workout_id: 3,
+      user_id: 3,
+      name: "Legs + Abs Workout",
+      description: "Workout focussing on the legs and abs.",
+      exercises: [exercises[6], exercises[7], exercises[8], exercises[9]], 
+    }),
+  ];
+  
+  
 
 
 const createWorkout = ({ workout_id, user_id, name, description}: Workout): Workout => {
