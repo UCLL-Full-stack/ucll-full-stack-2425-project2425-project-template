4f0ff46fcc88e6857ae4d3d7e8f71955c8f609d1
Rik Rosseel <contact@rikrosseel.com>
2024-11-01 22:53:01 +0100

    Add input validation and tests

--- back-end/model/speedrun.ts
+++ back-end/model/speedrun.ts
@@ -8,22 +8,22 @@ export class Speedrun {
     private time: number;
     private submitDate: Date;
     private videoLink: string;
-    private category: Category;
     private speedrunner: User;
     private isValidated: boolean;
     private validator?: User;
     private game: Game;
+    private category: Category;
 
     constructor(speedrun: {
         id?: number;
         time: number;
         submitDate: Date;
         videoLink: string;
-        category: Category;
         isValidated: boolean;
         speedrunner: User;
         validator?: User;
         game: Game;
+        category: Category;
     }) {
         this.validate(speedrun);
 
@@ -31,11 +31,11 @@ export class Speedrun {
         this.time = speedrun.time;
         this.submitDate = speedrun.submitDate;
         this.videoLink = speedrun.videoLink;
-        this.category = speedrun.category;
         this.speedrunner = speedrun.speedrunner;
         this.isValidated = speedrun.isValidated;
         this.validator = speedrun.validator;
         this.game = speedrun.game;
+        this.category = speedrun.category;
     }
 
     validate(speedrun: {
@@ -49,7 +49,50 @@ export class Speedrun {
         game: Game;
         category: Category;
     }) {
-        // throw new Error('Method not implemented.');
+        if (speedrun.time < 0) {
+            throw new Error('Speedrun time cannot be a negative number.');
+        }
+        if (!speedrun.submitDate) {
+            throw new Error('Submit date is required.');
+        }
+        if (!speedrun.videoLink?.trim()) {
+            throw new Error('Video link is required.');
+        }
+        /* canParse is found, but the method cannot be called. 
+        // if ('canParse' in URL) {
+        //     console.log('method found');
+        // }
+        // if (!URL.canParse(speedrun.videoLink)) {
+        //     throw new Error('Video link must be valid.');
+        // }
+        */
+        if (!this.validateVideoLink(speedrun.videoLink)) {
+            throw new Error('Video link must be valid.');
+        }
+        if (!speedrun.speedrunner) {
+            throw new Error('Speedrunner is required.');
+        }
+        if (speedrun.validator && !speedrun.isValidated) {
+            throw new Error('Speedruns must have a validator if it is validated.');
+        }
+        if (speedrun.isValidated && !speedrun.validator) {
+            throw new Error('Speedruns must be validated if validator is defined.');
+        }
+        if (!speedrun.game) {
+            throw new Error('Game is required.');
+        }
+        if (!speedrun.category) {
+            throw new Error('Category is required.');
+        }
+    }
+
+    validateVideoLink(url: string): boolean {
+        try {
+            new URL(url);
+            return true;
+        } catch (error) {
+            return false;
+        }
     }
 
     getId(): number | undefined {
--- back-end/test/model/speedrun.test.ts
+++ back-end/test/model/speedrun.test.ts
@@ -7,17 +7,6 @@ import { Speedrun } from '../../model/speedrun';
 const time: number = 1620000;
 const submitDate: Date = set(new Date(), { date: 24, month: 10, year: 2024 });
 const videoLink: string = 'https://www.example.com/lajksdf';
-const category: Category = new Category({
-    name: 'Any%',
-    description: 'Complete the game with any percent of completion.',
-    game: new Game({
-        name: 'Heavenly Bodies',
-        description:
-            'Heavenly Bodies is a game about cosmonauts, the body, and the absence of gravity.',
-        genre: 'Action, Adventure, Indie, Simulation',
-        releaseDate: set(new Date(), { date: 7, month: 12, year: 2021 }),
-    }),
-});
 const speedrunner: User = new User({
     username: 'speedy-gonzales',
     email: 'speedy.gonzales@email.com',
@@ -25,7 +14,7 @@ const speedrunner: User = new User({
     role: 'User',
     signUpDate: set(new Date(), { date: 29, month: 8, year: 1953 }),
 });
-const isValidated: boolean = false;
+const isValidated: boolean = true;
 const validator: User = new User({
     username: 'walter',
     email: 'walt.disney@email.com',
@@ -40,6 +29,17 @@ const game: Game = new Game({
     genre: 'Action, Adventure, Indie, Simulation',
     releaseDate: set(new Date(), { date: 7, month: 12, year: 2021 }),
 });
+const category: Category = new Category({
+    name: 'Any%',
+    description: 'Complete the game with any percent of completion.',
+    game: new Game({
+        name: 'Heavenly Bodies',
+        description:
+            'Heavenly Bodies is a game about cosmonauts, the body, and the absence of gravity.',
+        genre: 'Action, Adventure, Indie, Simulation',
+        releaseDate: set(new Date(), { date: 7, month: 12, year: 2021 }),
+    }),
+});
 
 test(`given: valid values for speedrun, when: speedrun is created, then: speedrun is created with those values`, () => {
     // given
@@ -49,11 +49,11 @@ test(`given: valid values for speedrun, when: speedrun is created, then: speedru
         time,
         submitDate,
         videoLink,
-        category,
         speedrunner,
         isValidated,
         validator,
         game,
+        category,
     });
     // then
     expect(speedrun.getTime()).toEqual(time);
@@ -66,19 +66,20 @@ test(`given: valid values for speedrun, when: speedrun is created, then: speedru
     expect(speedrun.getGame()).toEqual(game);
 });
 
-test(`given: valid values for speedrun and validator is undefined, when: speedrun is created, then: speedrun is created with those values`, () => {
+test(`given: valid values for speedrun, is not validated and validator is undefined, when: speedrun is created, then: speedrun is created with those values`, () => {
     // given
     const undefinedValidator: User | undefined = undefined;
+    const isNotValidated: boolean = false;
     // when
     const speedrun = new Speedrun({
         time,
         submitDate,
         videoLink,
-        category,
         speedrunner,
-        isValidated,
+        isValidated: isNotValidated,
         validator: undefinedValidator,
         game,
+        category,
     });
     // then
     expect(speedrun.getTime()).toEqual(time);
@@ -86,7 +87,187 @@ test(`given: valid values for speedrun and validator is undefined, when: speedru
     expect(speedrun.getVideoLink()).toEqual(videoLink);
     expect(speedrun.getCategory()).toEqual(category);
     expect(speedrun.getSpeedrunner()).toEqual(speedrunner);
-    expect(speedrun.getIsValidated()).toEqual(isValidated);
+    expect(speedrun.getIsValidated()).toEqual(isNotValidated);
     expect(speedrun.getValidator()).toEqual(undefinedValidator);
     expect(speedrun.getGame()).toEqual(game);
+});
+
+test(`given: invalid time, when: speedrun is created, then: an error is thrown`, () => {
+    // given
+    const invalidTime = -100;
+    // when
+    const createSpeedrun = () => {
+        new Speedrun({
+            time: invalidTime,
+            submitDate,
+            videoLink,
+            speedrunner,
+            isValidated,
+            validator,
+            game,
+            category,
+        });
+    };
+    // then
+    expect(createSpeedrun).toThrow('Speedrun time cannot be a negative number.');
+});
+
+test(`given: invalid submit date, when: speedrun is created, then: an error is thrown`, () => {
+    // given
+    const invalidSubmitDate: Date = null as any;
+    // when
+    const createSpeedrun = () => {
+        new Speedrun({
+            time,
+            submitDate: invalidSubmitDate,
+            videoLink,
+            speedrunner,
+            isValidated,
+            validator,
+            game,
+            category,
+        });
+    };
+    // then
+    expect(createSpeedrun).toThrow('Submit date is required.');
+});
+
+test(`given: empty video link, when: speedrun is created, then: an error is thrown`, () => {
+    // given
+    const invalidVideoLink = '  ';
+    // when
+    const createSpeedrun = () => {
+        new Speedrun({
+            time,
+            submitDate,
+            videoLink: invalidVideoLink,
+            speedrunner,
+            isValidated,
+            validator,
+            game,
+            category,
+        });
+    };
+    // then
+    expect(createSpeedrun).toThrow('Video link is required.');
+});
+
+test(`given: invalid video link, when: speedrun is created, then: an error is thrown`, () => {
+    // given
+    const invalidVideoLink = 'notaurl';
+    // when
+    const createSpeedrun = () => {
+        new Speedrun({
+            time,
+            submitDate,
+            videoLink: invalidVideoLink,
+            speedrunner,
+            isValidated,
+            validator,
+            game,
+            category,
+        });
+    };
+    // then
+    expect(createSpeedrun).toThrow('Video link must be valid.');
+});
+
+test(`given: invalid speedrunner, when: speedrun is created, then: an error is thrown`, () => {
+    // given
+    const InvalidSpeedrunner: User = null as any;
+    // when
+    const createSpeedrun = () => {
+        new Speedrun({
+            time,
+            submitDate,
+            videoLink,
+            speedrunner: InvalidSpeedrunner,
+            isValidated,
+            validator,
+            game,
+            category,
+        });
+    };
+    // then
+    expect(createSpeedrun).toThrow('Speedrunner is required.');
+});
+
+test(`given: is not validated but validator is defined, when: speedrun is created, then: an error is thrown`, () => {
+    // given
+    const isNotValidated: boolean = false;
+    // when
+    const createSpeedrun = () => {
+        new Speedrun({
+            time,
+            submitDate,
+            videoLink,
+            speedrunner,
+            isValidated: isNotValidated,
+            validator,
+            game,
+            category,
+        });
+    };
+    // then
+    expect(createSpeedrun).toThrow('Speedruns must have a validator if it is validated.');
+});
+
+test(`given: is validated but validator is undefined, when: speedrun is created, then: an error is thrown`, () => {
+    // given
+    const undefinedValidator: User | undefined = undefined;
+    // when
+    const createSpeedrun = () => {
+        new Speedrun({
+            time,
+            submitDate,
+            videoLink,
+            speedrunner,
+            isValidated,
+            validator: undefinedValidator,
+            game,
+            category,
+        });
+    };
+    // then
+    expect(createSpeedrun).toThrow('Speedruns must be validated if validator is defined.');
+});
+
+test(`given: invalid game, when: speedrun is created, then: an error is thrown`, () => {
+    // given
+    const invalidGame: Game = null as any;
+    // when
+    const createSpeedrun = () => {
+        new Speedrun({
+            time,
+            submitDate,
+            videoLink,
+            speedrunner,
+            isValidated,
+            validator,
+            game: invalidGame,
+            category,
+        });
+    };
+    // then
+    expect(createSpeedrun).toThrow('Game is required.');
+});
+
+test(`given: invalid category, when: speedrun is created, then: an error is thrown`, () => {
+    // given
+    const invalidCategory: Category = null as any;
+    // when
+    const createSpeedrun = () => {
+        new Speedrun({
+            time,
+            submitDate,
+            videoLink,
+            speedrunner,
+            isValidated,
+            validator,
+            game,
+            category: invalidCategory,
+        });
+    };
+    // then
+    expect(createSpeedrun).toThrow('Category is required.');
 });
