73678a717c59764f6159b021ff8946986624fc46
Efe Okan <efe-okan2004@hotmail.com>
2024-11-01 15:48:28 +0100

    Added links to groep 1 and 2 in home page

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -11,20 +11,36 @@ const Home: React.FC = () => {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <main >
+          <main>
         <span>
           <h1>Welkom op de agenda van Scouts Overijse!</h1>
         </span>
 
-          <div>
-              <p>
-                  Klik om de huidige activiteiten te zien
-              </p>
-              <Link href="/activiteiten" className="nav-link px-4 fs-5 text-white">
-                  Activiteiten
-              </Link>
-          </div>
-        </main>
+              <div>
+                  <p>
+                      Klik om de huidige activiteiten te zien
+                  </p>
+                  <Link href="/activiteiten" className="nav-link px-4 fs-5 text-white">
+                      Activiteiten
+                  </Link>
+              </div>
+              <div>
+                  <p>
+                      Klik om huidige activiteiten van groep 1 te zien
+                  </p>
+                  <Link href="/activiteiten/groep 1" className="nav-link px-4 fs-5 text-white">
+                      Activiteiten groep 1
+                  </Link>
+              </div>
+              <div>
+                  <p>
+                      Klik om huidige activiteiten van groep 2 te zien
+                  </p>
+                  <Link href="/activiteiten/groep 2" className="nav-link px-4 fs-5 text-white">
+                      Activiteiten groep 2
+                  </Link>
+              </div>
+          </main>
       </>
   );
 };
