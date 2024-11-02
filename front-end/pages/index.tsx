8f284b40fde6c6c5f79b9a2bbaed8f38c2a5998b
wooddebockUCLL <wood.debock@leerling.ucll.be>
2024-11-02 12:13:44 +0100

    frontend ready for changes

---
+++ front-end/pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Courses</title>
+        <meta name="description" content="Courses app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <span>
+          
+          <h1>Welcome!</h1>
+        </span>
+
+        <div>
+          <p>
+            Lets make some cocktails!
+          </p>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
