04163c863d931b47803c971d7bb5af7ab046e1f5
wooddebockUCLL <wood.debock@leerling.ucll.be>
2024-11-02 17:21:52 +0100

    The bannercycler

--- front-end/components/cocktail/CocktailContainer.tsx
+++ front-end/components/cocktail/CocktailContainer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Cocktail } from '@types'; 
-
+import { Cocktail } from '@types';
 
 const imgStyle = {
-    maxWidth: '300px',
-    maxHeight: '300px',
-    width: 'auto',
-    height: 'auto'
-  };
+  maxWidth: '300px',
+  maxHeight: '300px',
+  width: 'auto',
+  height: 'auto',
+  display: 'block',
+  margin: '0 auto 16px'
+};
 
 type Props = {
   cocktail: Cocktail; 
@@ -17,7 +18,7 @@ const CocktailContainer: React.FC<Props> = ({ cocktail }) => {
   return (
     <div className="cocktail-container">
       <h3>{cocktail.name}</h3>
-      <img src={cocktail.image} alt={cocktail.name} style={imgStyle}/>
+      <img src={cocktail.image} alt={cocktail.name} style={imgStyle} />
       <p><strong>Strongness :</strong> {cocktail.strongness}</p>
       <p><strong>Description :</strong> {cocktail.description}</p>
     </div>
--- front-end/components/header.tsx
+++ front-end/components/header.tsx
@@ -1,9 +1,27 @@
-
+import React, { useState } from 'react';
 
 const Header: React.FC = () => {
+  const banners = [
+    "banners/neonbanner.png",
+    "banners/firebanner.gif",
+    "banners/pinkbanner.gif",
+    "banners/goldbanner.png"
+  ];
+
+  const [currentBannerIndex, setCurrentBannerIndex] = useState(0);
+
+  const handleBannerClick = () => {
+    setCurrentBannerIndex((prevIndex) => (prevIndex + 1) % banners.length);
+  };
+
   return (
     <header style={{ textAlign: 'center', padding: '16px' }}>
-      <img src={"logo2.gif"} alt={"Sip Happens Online"} style={{ width: '1000px', height: 'auto' }} />
+      <img
+        src={banners[currentBannerIndex]}
+        alt="Sip Happens Online"
+        style={{ maxWidth: '1000px', height: '140px', cursor: 'pointer' }}
+        onClick={handleBannerClick}
+      />
     </header>
   );
 };
