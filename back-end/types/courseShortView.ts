01085188bc7e0648138727262d96921abdd6e834
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-11-02 21:21:46 +0000

    3/refactor

--- back-end/types/courseShortView.ts
+++ back-end/types/courseShortView.ts
@@ -1,10 +1,10 @@
 export default class Course {
-    public id: number;
+    public id?: number;
     public name: string;
     public phase: number;
     public credits: number;
 
-    constructor(course: { id: number; name: string; phase: number; credits: number}) {
+    constructor(course: { id?: number; name: string; phase: number; credits: number}) {
         this.id = course.id;
         this.name = course.name;
         this.phase = course.phase;
