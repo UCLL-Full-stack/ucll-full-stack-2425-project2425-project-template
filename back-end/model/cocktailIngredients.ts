b233557a08ff2ff0f7a7d4e81bfd6147c47ae31e
wooddebockUCLL <wood.debock@leerling.ucll.be>
2024-11-02 21:02:36 +0100

    Begin

---
+++ back-end/model/cocktailIngredients.ts
@@ -0,0 +1,11 @@
+import { Cocktail } from "./cocktail";
+import { Ingredient } from "./ingredient";
+
+class CocktailIngredient {
+    public cocktailId!: number;
+    public ingredientId!: number;
+    public amount!: string;
+
+}
+
+export default CocktailIngredient;
