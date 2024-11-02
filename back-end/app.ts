db4b0d690189a2f80642ad60daf672c4d6bef4eb
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-11-02 17:26:54 +0100

    feature: add error-handling middleware

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import { userRouter } from './controller/user.routes';
@@ -9,6 +9,12 @@ import { recipeRouter } from './controller/recipe.routes';
 const app = express();
 dotenv.config();
 const port = process.env.APP_PORT || 3000;
+
+// Error-handling middleware
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack); // logs the error
+    res.status(500).json({ message: 'Something went wrong!' }); // sends an error response
+});
 
 app.use(cors());
 app.use(bodyParser.json());
