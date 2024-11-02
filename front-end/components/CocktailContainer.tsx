1cc5c81f1a43998b46e0dacf4458c224ecd65e12
wooddebockUCLL <wood.debock@leerling.ucll.be>
2024-11-02 15:46:06 +0100

    Front the end Some pictures need new paths

--- front-end/components/CocktailContainer.tsx
+++ front-end/components/CocktailContainer.tsx
@@ -10,6 +10,7 @@ const CocktailContainer: React.FC<Props> = ({ cocktail }) => {
   return (
     <div className="cocktail-container">
       <h3>{cocktail.name}</h3>
+      <img src={cocktail.image} alt={cocktail.name} />
       <p><strong>Strongness :</strong> {cocktail.strongness}</p>
       <p><strong>Description :</strong> {cocktail.description}</p>
     </div>
--- front-end/types/index.ts
+++ front-end/types/index.ts
@@ -10,4 +10,5 @@ export type Cocktail = {
     description: string;
     strongness: number;
     ingredientsList: Ingredient[];
+    image: string;
 }
