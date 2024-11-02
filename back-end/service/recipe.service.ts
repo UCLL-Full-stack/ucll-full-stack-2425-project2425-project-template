167f01e93db64017eccab46c5337e1cec8439d92
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-11-02 16:10:39 +0100

    delete unused methods

--- back-end/service/recipe.service.ts
+++ back-end/service/recipe.service.ts
@@ -1,26 +1,6 @@
 import { Recipe } from '../model/recipe';
 import recipeDb from '../repository/recipe.db';
 
-/* Controller endpoints:
-
-1. Create a new recipe 
-POST /recipes
-Body: recipe data
-
-2. Get a specific recipe
-GET /recipes/:id
-
-3. Update a recipe
-PUT /recipes/:id
-Body: updated recipe data
-
-4. Delete a recipe
-DELETE /recipes/:id
-
-5. Get all recipes
-GET /recipes
-*/
-
 const getAllRecipes = (): Recipe[] => recipeDb.getAllRecipes();
 
 const getRecipeById = (id: number): Recipe => {
@@ -29,13 +9,4 @@ const getRecipeById = (id: number): Recipe => {
     return recipe;
 };
 
-const createRecipe = ({
-    title,
-    instructions,
-    cookingTime,
-    category,
-    ingredients,
-}: NewRecipeInput): Recipe => {
-    const recipe = new Recipe({ title, instructions, cookingTime, category, ingredients });
-    return recipeDb.addRecipe(recipe);
-};
+export default { getRecipeById };
