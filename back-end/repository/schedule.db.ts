614002589edfbb95d8bb1a83827023cfb22d8555
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-11-02 16:10:15 +0100

    create temporary mock data to test

--- back-end/repository/schedule.db.ts
+++ back-end/repository/schedule.db.ts
@@ -33,14 +33,26 @@ const ingredient2 = new Ingredient({
     category: 'Pantry' as IngredientCategory,
     recipes: [],
 });
+const ingredient3 = new Ingredient({
+    id: 3,
+    name: 'Chicken Breast',
+    category: 'Meat & Fish' as IngredientCategory,
+    recipes: [],
+});
+const ingredient4 = new Ingredient({
+    id: 4,
+    name: 'Lettuce',
+    category: 'Produce' as IngredientCategory,
+    recipes: [],
+});
 
-// Mock Recipe
+// Mock Recipes
 const recipe1 = new Recipe({
     id: 1,
     title: 'Spaghetti Bolognese',
     instructions: 'Cook pasta, prepare sauce, mix together.',
     cookingTime: 30,
-    category: 'Dinner',
+    category: 'dinner',
     ingredients: [
         new RecipeIngredient({
             recipe: null as unknown as Recipe,
@@ -56,19 +68,105 @@ const recipe1 = new Recipe({
         }),
     ],
     user: user1,
+    imageUrl:
+        'https://images.unsplash.com/photo-1622973536968-3ead9e780960?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    isFavorite: true,
 });
+
+const recipe2 = new Recipe({
+    id: 2,
+    title: 'Chicken Salad',
+    instructions: 'Grill chicken, chop lettuce, mix together.',
+    cookingTime: 20,
+    category: 'lunch',
+    ingredients: [
+        new RecipeIngredient({
+            recipe: null as unknown as Recipe,
+            ingredient: ingredient3,
+            unit: 'g',
+            quantity: 150,
+        }),
+        new RecipeIngredient({
+            recipe: null as unknown as Recipe,
+            ingredient: ingredient4,
+            unit: 'g',
+            quantity: 100,
+        }),
+    ],
+    user: user1,
+    imageUrl:
+        'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    isFavorite: false,
+});
+
 recipe1.getIngredients()?.forEach((ingredient) => {
     ingredient.setRecipe(recipe1);
-}); // Set recipe references in ingredients
+});
+recipe2.getIngredients()?.forEach((ingredient) => {
+    ingredient.setRecipe(recipe2);
+});
 
-// Mock schedule
+// Mock schedules
 const schedule1 = new Schedule({
     id: 1,
     user: user1,
     date: new Date('2024-11-03'),
-    recipes: [recipe1],
+    recipes: [recipe1, recipe2],
 });
-const schedules: Schedule[] = [schedule1]; // Add schedules to the mock data array
+
+const recipe3 = new Recipe({
+    id: 3,
+    title: 'Grilled Cheese Sandwich',
+    instructions:
+        'Grill cheese between slices of bread, Butter the outside of the bread, Heat a skillet over medium heat, Cook until golden brown on both sides',
+    cookingTime: 10,
+    category: 'lunch',
+    ingredients: [
+        new RecipeIngredient({
+            recipe: null as unknown as Recipe,
+            ingredient: ingredient1,
+            unit: 'slices',
+            quantity: 2,
+        }),
+        new RecipeIngredient({
+            recipe: null as unknown as Recipe,
+            ingredient: ingredient2,
+            unit: 'slices',
+            quantity: 2,
+        }),
+        new RecipeIngredient({
+            recipe: null as unknown as Recipe,
+            ingredient: ingredient3,
+            unit: 'tablespoons',
+            quantity: 1,
+        }),
+        new RecipeIngredient({
+            recipe: null as unknown as Recipe,
+            ingredient: ingredient4,
+            unit: 'tablespoons',
+            quantity: 1,
+        }),
+    ],
+    user: user1,
+    imageUrl:
+        'https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1173&q=80',
+    isFavorite: true,
+    notes: 'Make sure to use fresh bread for the best taste. You can add tomatoes or ham for extra flavor.',
+    source: 'https://www.example.com/grilled-cheese-sandwich-recipe',
+});
+
+recipe3.getIngredients()?.forEach((ingredient) => {
+    ingredient.setRecipe(recipe3);
+});
+
+const schedule2 = new Schedule({
+    id: 2,
+    user: user1,
+    date: new Date('2024-11-04'),
+    recipes: [recipe3],
+});
+
+const schedules: Schedule[] = [schedule1, schedule2]; // Add schedules to the mock data array
 
 const getScheduleByUserIdAndDate = (userId: number, date: Date): Schedule | null => {
     return (
