0f6a34a9793fe51732630c5e67b5e8ef7c40cb05
Timothy De Jaeger <timothy.dejaeger@student.ucll.be>
2024-11-02 20:36:30 +0100

    added types

---
+++ front-end/types/index.ts
@@ -0,0 +1,45 @@
+type User = {
+    id?: number;
+    nationalRegisterNumber: string;
+    name: string;
+    birthDate: Date | null;
+    isAdministrator: boolean;
+    phoneNumber: string;
+    email: string;
+    password: string;
+};
+
+type Account = {
+    id?: number;
+    accountNumber?: string;
+    balance?: number;
+    isShared: boolean;
+    startDate?: Date;
+    endDate?: Date | null;
+    status?: string;
+    type: string;
+    transactions?: Transaction[];
+    users: User[];
+    budgetgoals?: BudgetgoalInput[];
+};
+
+type Transaction = {
+    id?: number;
+    referenceNumber?: string;
+    date?: Date;
+    amount: number;
+    currency: string;
+    type: string;
+    account: Account;
+};
+
+type BudgetgoalInput = {
+    id?: number;
+    name: string;
+    amount: number;
+    startDate: Date;
+    endDate: Date;
+    account: Account;
+};
+
+export type { User, Transaction, BudgetgoalInput, Account };
