9e5fc0cd0b0a861da25cc484eddaed32b4d2404f
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2024-11-03 14:23:56 +0000

    29/ added tailwind

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#776E87",
+        secondary: "#4D4861",
+        success: "#DE9151",
+        danger: "#E00049",
+        safe: "#5CDE51",
+        alter: "#C251DE",
+      },
+      boxShadow: {
+        button: "1px 2px 4px rgba(0, 0, 0, 0.8)",
+        button_pressed: "inset 1px 2px 4px rgba(0, 0, 0, 0.8)"
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
