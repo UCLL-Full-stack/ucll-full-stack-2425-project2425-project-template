5249c3692c1ca20ad02135e2736271ed358f6de5
SlipknotFan666 <timo.dewinter@student.ucll.be>
2024-11-03 14:00:23 +0100

    added swagger stuff to the app.ts and routes

--- back-end/app.ts
+++ back-end/app.ts
@@ -4,6 +4,14 @@ import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import userRouter from './controller/User.routes'
+import recipeRouter from './controller/Recipe.routes'
+import ingredientRouter from './controller/Ingredient.routes'
+import reviewRouter from './controller/Review.routes'
+import { expressjwt } from 'express-jwt';
+import { Request, Response, NextFunction } from 'express';
+
+
 
 const app = express();
 dotenv.config();
@@ -11,6 +19,26 @@ const port = process.env.APP_PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/user', userRouter);
+app.use('/recipe', recipeRouter);
+app.use('/ingredient', ingredientRouter);
+app.use('/review', reviewRouter);
+
+const swaggerOpts = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'TasteBuddy API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./controller/*.routes.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOpts);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 app.get('/status', (req, res) => {
     res.json({ message: 'Back-end is running...' });
