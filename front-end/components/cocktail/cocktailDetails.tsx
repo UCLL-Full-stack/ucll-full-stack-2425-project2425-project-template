8fa1080551b8f4d8927aef47968e7700197e4a85
wooddebockUCLL <wood.debock@leerling.ucll.be>
2024-11-03 03:05:12 +0100

    puh

--- front-end/components/cocktail/cocktailDetails.tsx
+++ front-end/components/cocktail/cocktailDetails.tsx
@@ -15,32 +15,13 @@ const imgStyle = {
 
 const CocktailDetails: React.FC<Props> = ({ cocktail }: Props) => {
     return (
-        <>
+        
         <div className="cocktail-details">
             <h3>{cocktail.name}</h3>
             <img src={cocktail.image} alt={cocktail.name} style={imgStyle} />
             <p><strong>Strongness :</strong> {cocktail.strongness}</p>
             <p><strong>Description :</strong> {cocktail.description}</p>
-            {/* <table>
-                <thead>
-                    <tr>
-                        <th>Ingredient</th>
-                        <th>Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-             {cocktail.ingredientsList.map((ingredient, index) => (
-              <tr key={index}>
-                  <td className="ingredient-cell">{ingredient.name}</td>
-                   <td className="ingredient-cell">{ingredient.amount}</td>
-              </tr>
-                  ))}
-                </tbody>
-            </table> */}
         </div>
-        
-        
-        </>
         );
     }
 
