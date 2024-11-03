f076045b733437c20ed28f67cb1cb18f010b5e46
Amelie De Cre <amelie.decre@gmail.com>
2024-11-03 13:48:08 +0100

    User post request zonder swagger.

--- back-end/controller/user.routes.ts
+++ back-end/controller/user.routes.ts
@@ -1,4 +1,17 @@
 import express, { NextFunction, Request, Response } from 'express';
 import userService from '../service/user.service';
+import { UserInput } from '../types';
 
 const userRouter = express.Router();
+
+userRouter.post("/", async (req:Request, res:Response) => {
+    try {
+        const user = <UserInput>req.body;
+        const result = await userService.createUser(user);
+        res.status(200).json(result);
+    } catch (error: any) {
+        res.status(400).json({status: 'error', errorMessage: error.errorMessage})
+    }
+})
+
+export { userRouter };
--- back-end/service/user.service.ts
+++ back-end/service/user.service.ts
@@ -1,3 +1,25 @@
+import { id } from 'date-fns/locale';
 import { User } from '../model/user';
+import userDb from '../repository/user.db';
+import { UserInput } from '../types';
 
-export default {}   
+const createUser = ({username, email, password}: UserInput): User => {
+
+    if (password.length !== 8) {
+        throw new Error("Password needs to be 8 characters long.");
+    }
+
+    const user = new User({
+        username,
+        email,
+        password
+    });
+
+    return userDb.createUser({
+        username: user.getUsername(),
+        email: user.getEmail(),
+        password: user.getPassword()
+    })
+}
+
+export default {createUser}   
