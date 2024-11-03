8e02de9c92b21b6da9899554481329b4df76363b
Nina de Weerd <nina.deweerd@student.ucll.be>
2024-11-03 15:15:26 +0100

    Fixed price issue and added more validation

--- back-end/repository/ingredient.db.ts
+++ back-end/repository/ingredient.db.ts
@@ -22,6 +22,34 @@ const ingredienten = [
         aantal: 200,
         prijs: 1.32
     }),
+    new Ingredient({
+        id: 4,
+        naam: 'Tuna',
+        type: 'Protein',
+        aantal: 50,
+        prijs: 3.61
+    }),
+    new Ingredient({
+        id: 5,
+        naam: 'Corn',
+        type: 'Topping',
+        aantal: 198,
+        prijs: 0.54
+    }),
+    new Ingredient({
+        id: 6,
+        naam: 'Seaweed',
+        type: 'Topping',
+        aantal: 228,
+        prijs: 1.09
+    }),
+    new Ingredient({
+        id: 7,
+        naam: 'Srirachia',
+        type: 'Sauce',
+        aantal: 450,
+        prijs: 1.14
+    })
 ];
 
 const getAllIngredienten = (): Ingredient[] => ingredienten;
@@ -30,4 +58,8 @@ const addIngredient = (ingredient: Ingredient) => {
     ingredienten.push(ingredient);
 };
 
-export default { getAllIngredienten, addIngredient }
+const getIngredientById = ({ id }: { id: number }): Ingredient | null => {
+    return ingredienten.find((ingredient) => ingredient.getId() === id) || null;
+}
+
+export default { getAllIngredienten, addIngredient, getIngredientById }
--- back-end/service/pokebowl.service.ts
+++ back-end/service/pokebowl.service.ts
@@ -1,11 +1,13 @@
 import { Ingredient } from "../model/ingredient";
 import { Pokebowl } from "../model/pokebowl";
+import ingredientDb from "../repository/ingredient.db";
 import pokebowlDb from "../repository/pokebowl.db";
 import { PokebowlInput } from "../types";
+import ingredientService from "./ingredient.service";
 
 const getAllPokebowls = async (): Promise<Pokebowl[]> => pokebowlDb.getAllPokebowls();
 
-const createPokebowl = async ({ naam, type, beschrijving, maxAantalIngredienten, ingredienten }: PokebowlInput): Promise<Pokebowl> => {
+const createPokebowl = async ({ naam, type, beschrijving, prijs, maxAantalIngredienten, ingredienten }: PokebowlInput): Promise<Pokebowl> => {
 
     const lengte = (await getAllPokebowls()).length
 
@@ -13,14 +15,25 @@ const createPokebowl = async ({ naam, type, beschrijving, maxAantalIngredienten,
         id: lengte + 1,
         naam: naam,
         type: type,
+        prijs: prijs,
         beschrijving: beschrijving,
         maxAantalIngredienten: maxAantalIngredienten,
         ingredienten: []
     });
 
     ingredienten.forEach((ingr) => {
-        const nieuweIngredient = new Ingredient(ingr);
-        pokebowl.addIngredient(nieuweIngredient);
+        if (!ingr.id) {
+            throw new Error('Ingredient id is required');
+        }
+        const ingrId = ingredientDb.getIngredientById({ id: ingr.id });
+
+        if (!ingrId) {
+            throw new Error("Ingredient not found")
+        } else {
+            // const nieuweIngredient = new Ingredient(ingr);
+            pokebowl.addIngredient(ingrId);
+        }
+
     })
     pokebowlDb.createPokebowl(pokebowl);
     pokebowl.calculatePrice();
