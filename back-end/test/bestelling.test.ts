71c25cc8298cda7b98d0c1e66e244feec8e8d29a
Nina de Weerd <nina.deweerd@student.ucll.be>
2024-11-03 15:43:43 +0100

    changed the tests

--- back-end/test/bestelling.test.ts
+++
@@ -1,8 +0,0 @@
-import { Bestelling } from "../model/bestelling";
-
-test("given: valid values for bestelling, when: creating bestelling, then: bestelling is created", () => {
-    const datum1 = new Date('2024-10-24')
-    const bestelling = new Bestelling({ datum: datum1})
-
-    expect(bestelling.getDatum()).toEqual(datum1);
-})
