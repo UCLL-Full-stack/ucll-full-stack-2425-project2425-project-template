84cba086bc8b5d075742ed104e1e7c42bb46f5eb
Dean Duraku <dean.duraku@hotmail.com>
2024-11-03 18:03:31 +0100

    carService frontend add en delete functies

--- front-end/services/CarService.ts
+++ front-end/services/CarService.ts
@@ -1,13 +1,37 @@
+import { Car } from "@/types";
+
 const getAllCars = async () => {
-    return await fetch(process.env.NEXT_PUBLIC_API_URL + '/cars', 
-      {
-        method: "GET",
-         headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  };
-  const carService = {
-    getAllCars,
-  }
-  export default carService;
+  return await fetch(process.env.NEXT_PUBLIC_API_URL + '/cars',
+    {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+};
+const addCar = async (carData: Car) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/cars`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(carData),
+  });
+  const newCar = await response.json();
+  return newCar;
+};
+const deleteCar = async (carId: number) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/cars/${carId}`, {
+    method: "DELETE",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const carService = {
+  getAllCars,
+  addCar,
+  deleteCar,
+}
+export default carService;
