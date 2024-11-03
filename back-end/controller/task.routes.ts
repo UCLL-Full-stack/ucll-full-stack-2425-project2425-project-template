68d520d48176cb76200e320d5b6b2d93a64137d2
Alan-Kim <r0949915@ucll.be>
2024-11-03 16:13:17 +0100

    task swagger done. All endpoints requested by teammate completed including swagger, service and domain validation, moving on to front-end mess cleaning and creating

--- back-end/controller/task.routes.ts
+++ back-end/controller/task.routes.ts
@@ -3,6 +3,77 @@ import taskService from '../service/task.service';
 
 const taskRouter = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Task:
+ *       type: object
+ *       properties:
+ *         taskId:
+ *           type: string
+ *           description: Unique identifier for the task
+ *         title:
+ *           type: string
+ *           description: Title of the task
+ *         description:
+ *           type: string
+ *           description: Description of the task
+ *         dueDate:
+ *           type: string
+ *           format: date-time
+ *           description: Due date of the task
+ *         assignees:
+ *           type: array
+ *           items:
+ *             type: string
+ *             description: List of user IDs assigned to the task
+ * 
+ *     ErrorResponse:
+ *       type: object
+ *       properties:
+ *         error:
+ *           type: string
+ *           description: Error message
+ */
+
+
+/**
+ * @swagger
+ * /api/tasks/{taskId}:
+ *   put:
+ *     summary: Update a task
+ *     parameters:
+ *       - in: path
+ *         name: taskId
+ *         required: true
+ *         description: The ID of the task to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       200:
+ *         description: Task updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Task updated successfully
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 taskRouter.put('/:taskId', (req, res) => {
     const { taskId } = req.params;
     const updatedTask = req.body;
@@ -18,6 +89,28 @@ taskRouter.put('/:taskId', (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/tasks/{taskId}:
+ *   delete:
+ *     summary: Delete a task
+ *     parameters:
+ *       - in: path
+ *         name: taskId
+ *         required: true
+ *         description: The ID of the task to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Task deleted successfully
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ */
 taskRouter.delete('/:taskId', (req, res) => {
     const { taskId } = req.params;
     try {
