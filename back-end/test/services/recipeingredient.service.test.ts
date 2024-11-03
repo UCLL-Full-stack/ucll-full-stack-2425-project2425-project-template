6a49d6b33d31f76a461f068c9253df1ef12c264a
thomasgorisucll <79001636+thomasgorisucll@users.noreply.github.com>
2024-11-03 21:21:05 +0100

    Something

---
+++ back-end/test/services/recipeingredient.service.test.ts
@@ -0,0 +1,12 @@
+const recipeingredientid = 1
+const recipeid = 1
+const ingredientid = 1
+const unit = "grams"
+const quantity = 200
+
+let mockRecipeIngredientGetRecipeIngredientById: jest.Mock;
+
+beforeEach(() => {
+    mockRecipeIngredientGetRecipeIngredientById = jest.fn()
+})
+
