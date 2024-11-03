2edf249d8deb97b8084223e8cacabc523cdd3054
Amelie De Cre <amelie.decre@gmail.com>
2024-11-03 20:07:00 +0100

    swagger

--- back-end/controller/user.routes.ts
+++ back-end/controller/user.routes.ts
@@ -1,8 +1,64 @@
+/**
+ * @swagger
+ *   components:
+ *    securitySchemes:
+ *     bearerAuth:
+ *      type: http
+ *      scheme: bearer
+ *      bearerFormat: JWT
+ *    schemas:
+ *      User:
+ *          type: object
+ *          properties:
+ *            id:
+ *              type: number
+ *              format: int64
+ *            username:
+ *              type: string
+ *              description: Username.
+ *            email:
+ *              type: string
+ *              description: User email.
+ *            password:
+ *              type: string
+ *              description: User password.
+ *      UserInput:
+ *          type: object
+ *          properties:
+ *              username:
+ *                  type: string
+ *              email:
+ *                  type: string
+ *              password:
+ *                  type: string
+ */
+
+
 import express, { NextFunction, Request, Response } from 'express';
 import userService from '../service/user.service';
 import { UserInput } from '../types';
 
 const userRouter = express.Router();
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *      summary: Create a new user.
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/UserInput'
+ *      responses:
+ *         200:
+ *            description: created user.
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#/components/schemas/User'
+ */
 
 userRouter.post("/", async (req:Request, res:Response) => {
     try {
