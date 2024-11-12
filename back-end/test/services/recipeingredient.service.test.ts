e0eece43fff602107bd79fec09afe1827f72d8be
thomasgorisucll <79001636+thomasgorisucll@users.noreply.github.com>
2024-11-12 20:59:04 +0100

    fix spelling mistakes

--- back-end/test/services/recipeingredient.service.test.ts
+++ back-end/test/services/recipeingredient.service.test.ts
@@ -1,13 +1,13 @@
 import { RecipeIngredient } from "../../model/recipeingredient"
 import ingredientDb from "../../repository/ingredient.db"
 
-const recipeingredientid = 1
-const recipeid = 1
-const ingredientid = 1
+const recipeIngredientId = 1
+const recipeId = 1
+const ingredientId = 1
 const unit = "grams"
 const quantity = 200
-const recipeingredient = new RecipeIngredient({
-    recipeingredientId: 1,
+const recipeIngredient = new RecipeIngredient({
+    recipeIngredientId: 1,
     recipeId: 1,
     ingredientId: 1,
     unit: unit,
@@ -24,12 +24,12 @@ afterEach(() => {
     jest.clearAllMocks()
 })
 
-test('given existing recipeingredients, when fetching an existing recipeingredient by id, then return the correct recipeingredients', () => {
+test('given existing recipeIngredient, when fetching an existing recipeIngredient by id, then return the correct recipeIngredient', () => {
     //given
-    const recipeingredientone = new RecipeIngredient({ recipeingredientId: 1, recipeId: 1, ingredientId: 1, unit: "grams", quantity: 250 })
-    ingredientDb.getRecipeIngredientById = mockRecipeIngredientGetRecipeIngredientById.mockReturnValue(recipeingredientone)
+    const recipeIngredient1 = new RecipeIngredient({ recipeIngredientId: 1, recipeId: 1, ingredientId: 1, unit: "grams", quantity: 250 })
+    ingredientDb.getRecipeIngredientById = mockRecipeIngredientGetRecipeIngredientById.mockReturnValue(recipeIngredient1)
     //when
     const result = ingredientDb.getRecipeIngredientById({ id: 1 })
     //then
-    expect(result).toEqual(recipeingredientone)
+    expect(result).toEqual(recipeIngredient1)
 })
