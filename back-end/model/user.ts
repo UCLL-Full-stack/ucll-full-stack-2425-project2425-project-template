95cee33e147c2d2c08d009988d2446d479c614a1
thomasgorisucll <79001636+thomasgorisucll@users.noreply.github.com>
2024-11-12 21:24:23 +0100

    removed the _ removed the setters

--- back-end/model/user.ts
+++ back-end/model/user.ts
@@ -1,13 +1,13 @@
 import { Role } from '../types';
 
 export class User {
-    private _userId?: number;
-    private _username: string;
-    private _firstName: string;
-    private _lastName: string;
-    private _email: string;
-    private _password: string;
-    private _role: Role;
+    private userId?: number;
+    private username: string;
+    private firstName: string;
+    private lastName: string;
+    private email: string;
+    private password: string;
+    private role: Role;
 
     constructor(user: {
         id?: number;
@@ -18,77 +18,51 @@ export class User {
         password: string;
         role: Role;
     }) {
-        this._userId = user.id;
-        this._username = user.username;
-        this._firstName = user.firstName;
-        this._lastName = user.lastName;
-        this._email = user.email;
-        this._password = user.password;
-        this._role = user.role;
+        this.userId = user.id;
+        this.username = user.username;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.email = user.email;
+        this.password = user.password;
+        this.role = user.role;
     }
 
     public getUserId(): number | undefined {
-        return this._userId;
+        return this.userId;
     }
-
-    // no setter need for the id.
 
     public getUsername(): string {
-        return this._username;
-    }
-
-    public setUsername(username: string): void {
-        this._username = username;
+        return this.username;
     }
 
     public getFirstName(): string {
-        return this._firstName;
-    }
-
-    public setFirstName(firstName: string): void {
-        this._firstName = firstName;
+        return this.firstName;
     }
 
     public getLastName(): string {
-        return this._lastName;
-    }
-
-    public setLastName(lastName: string): void {
-        this._lastName = lastName;
+        return this.lastName;
     }
 
     public getEmail(): string {
-        return this._email;
-    }
-
-    public setEmail(email: string): void {
-        this._email = email;
+        return this.email;
     }
 
     public getPassword(): string {
-        return this._password;
-    }
-
-    public setPassword(password: string): void {
-        this._password = password;
+        return this.password;
     }
 
     getRole(): Role {
-        return this._role;
-    }
-
-    public setRole(role: Role): void {
-        this._role = role;
+        return this.role;
     }
 
     equal(user: User): boolean {
         return (
-            this._username === user.getUsername() &&
-            this._firstName === user.getFirstName() &&
-            this._lastName === user.getLastName() &&
-            this._email === user.getEmail() &&
-            this._password === user.getPassword() &&
-            this._role === user.getRole()
+            this.username === user.getUsername() &&
+            this.firstName === user.getFirstName() &&
+            this.lastName === user.getLastName() &&
+            this.email === user.getEmail() &&
+            this.password === user.getPassword() &&
+            this.role === user.getRole()
         );
     }
 }
