44d4345f72b644354466e82397c776c1e1589d52
thomasgorisucll <79001636+thomasgorisucll@users.noreply.github.com>
2024-11-12 21:21:27 +0100

    removed the _ removed the setters removed unNeeded imports

--- back-end/model/tags.ts
+++ back-end/model/tags.ts
@@ -1,44 +1,32 @@
-import { Recipe } from "./recipe";
-
 export class Tag {
-    private _tagId?: number;
-    private _name: string;
-    private _description: string;
+    private tagId?: number;
+    private name: string;
+    private description: string;
 
     constructor(tag: { tagId?: number, name: string, description: string }) {
-        this._tagId = tag.tagId;
-        this._name = tag.name;
-        this._description = tag.description;
+        this.tagId = tag.tagId;
+        this.name = tag.name;
+        this.description = tag.description;
     }
 
     getTagId(): number | undefined {
-        return this._tagId;
+        return this.tagId;
     }
-    // No setter of id need this database will set it. 
 
     getName(): string {
-        return this._name;
-    }
-
-    setName(name: string): void {
-        this._name = name;
+        return this.name;
     }
 
     getDescription(): string {
-        return this._description;
+        return this.description;
     }
-
-    setDescription(description: string): void {
-        this._description = description;
-    }
-
 
     //I haven't included the Id because this will normally never match 
     //I also haven't included the recipes because if the have the same name and description the are still the same
     equals(tag: Tag): boolean {
         return(
-            this._name === tag._name &&
-            this._description === tag._description 
+            this.name === tag.name &&
+            this.description === tag.description
         )
     }
 }
