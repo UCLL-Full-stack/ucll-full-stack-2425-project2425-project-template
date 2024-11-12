6bf143b76c031a59633edd79f4489210c0d7d338
thomasgorisucll <79001636+thomasgorisucll@users.noreply.github.com>
2024-11-12 21:12:27 +0100

    removed the not needed _ changed recipeIk for Recipe

--- back-end/model/recipeingredient.ts
+++ back-end/model/recipeingredient.ts
@@ -1,48 +1,45 @@
 import {Ingredient} from "./ingredient";
+import {Recipe} from "./recipe";
 
 export class RecipeIngredient {
-    private _recipeIngredientId?: number;
-    private _recipeId: number;
-    private _ingredientId: number;
-    private _unit: string;
-    private _quantity: number;
+    private recipeIngredientId?: number;
+    private recipe: Recipe;
     private ingredient: Ingredient;
+    private unit: string;
+    private quantity: number;
+
 
 
     constructor(recipeIngredient: {
         recipeIngredientId?: number;
-        recipeId: number;
-        ingredientId: number;
+        recipe: Recipe
+        ingredient: Ingredient
         unit: string;
         quantity: number;
-        ingredient: Ingredient
+
     }) {
-        this._recipeIngredientId = recipeIngredient.recipeIngredientId;
-        this._recipeId = recipeIngredient.recipeId;
-        this._ingredientId = recipeIngredient.ingredientId;
-        this._unit = recipeIngredient.unit;
-        this._quantity = recipeIngredient.quantity;
+        this.recipeIngredientId = recipeIngredient.recipeIngredientId;
+        this.recipe = recipeIngredient.recipe;
+        this.unit = recipeIngredient.unit;
+        this.quantity = recipeIngredient.quantity;
         this.ingredient = recipeIngredient.ingredient;
     }
 
     getRecipeIngredientId(): number| undefined {
-        return this._recipeIngredientId;
+        return this.recipeIngredientId;
     }
 
-    getRecipeId(): number {
-        return this._recipeId;
+    getRecipe(): Recipe {
+        return this.recipe;
     }
 
-    getIngredientId(): number {
-        return this._ingredientId;
-    }
 
     getUnit(): string {
-        return this._unit;
+        return this.unit;
     }
 
     getQuantity(): number {
-        return this._quantity;
+        return this.quantity;
     }
 
     getIngredient():Ingredient {
