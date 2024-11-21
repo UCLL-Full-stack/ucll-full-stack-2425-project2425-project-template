21c0b35197568b3623a2183bc9122f60acf1927f
XanderDhondtUCLL <xander.dhondt@live.be>
2024-11-21 10:07:46 +0100

    managed to almost finish seed

--- back-end/model/line.ts
+++ back-end/model/line.ts
@@ -1,21 +1,16 @@
-import { 
-    Line as LinePrisma,
-} from "@prisma/client"; 
+import { Floor, Line as LinePrisma } from '@prisma/client';
 
 export class Line {
     private id?: number;
     private tiles: string[];
     private lineNum: number;
+    private floorId: number;
 
-
-    constructor(line: {
-        id?: number;
-        tiles: string[];
-        lineNum: number;
-    }) {
+    constructor(line: { id?: number; tiles: string[]; lineNum: number; floorId: number }) {
         this.id = line.id;
         this.lineNum = line.lineNum;
         this.tiles = line.tiles;
+        this.floorId = line.floorId;
     }
 
     getId(): number | undefined {
@@ -30,19 +25,16 @@ export class Line {
         return this.tiles;
     }
 
-    setTile(j: number, input: string){
+    setTile(j: number, input: string) {
         this.tiles[j] = input;
     }
 
-    static from({
-        id,
-        tiles,
-        lineNum,
-    }: LinePrisma) {
+    static from({ id, tiles, lineNum, floorId }: LinePrisma) {
         return new Line({
             id,
             tiles,
             lineNum,
-        })
+            floorId,
+        });
     }
 }
