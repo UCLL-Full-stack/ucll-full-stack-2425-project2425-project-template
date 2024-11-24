819bee07a21d21dd1089601e69e0a7d4e2a65979
MilanVermeirenUCLL <r0932151@ucll.be>
2024-11-24 14:48:02 +0100

    update shopping cart model

--- back-end/model/shoppingcart.ts
+++ back-end/model/shoppingcart.ts
@@ -1,38 +1,59 @@
+import { CartItem } from "./cartItem";
 import { Product } from "./product";
 
 export class Shoppingcart {
     private id?: number;
-    private products: Product[];
+    private cartItems: CartItem[];
     private totalPrice: number;
 
-    constructor(shoppingcart:{id?: number, products: Product[], totalPrice: number}) {
+    constructor(shoppingcart: { id?: number; cartItems: CartItem[] }) {
         this.id = shoppingcart.id;
-        this.products = shoppingcart.products;
-        this.totalPrice = shoppingcart.totalPrice;
+        this.cartItems = shoppingcart.cartItems;
+        this.totalPrice = this.calculateTotalPrice();
     }
 
-    addProductToShoppingCart(product: Product) {
-        this.products.push(product);
-        this.totalPrice += product.getPrice();
+    public addProductToCart(product: Product, quantity: number): void {
+        const existingCartItem = this.cartItems.find(
+            (item) => item.getProduct().equals(product)
+        );
+
+        if (existingCartItem) {
+            existingCartItem.setQuantity(existingCartItem.getQuantity() + quantity);
+        } else {
+            this.cartItems.push(new CartItem({ product, quantity }));
+        }
+
+        this.totalPrice = this.calculateTotalPrice();
+    }
+
+    public removeProductFromCart(product: Product): void {
+        this.cartItems = this.cartItems.filter(
+            (item) => !item.getProduct().equals(product)
+        );
+        this.totalPrice = this.calculateTotalPrice();
+    }
+
+    public calculateTotalPrice(): number {
+        return this.cartItems.reduce((sum, item) => sum + item.getTotalPrice(), 0);
+    }
+
+    public getCartItems(): CartItem[] {
+        return this.cartItems;
+    }
+
+    public getTotalPrice(): number {
+        return this.totalPrice;
     }
 
     public getId(): number | undefined {
         return this.id;
     }
 
-    public getProducts(): Product[] {
-        return this.products;
-    }
-
-    public getTotalPrice(): number {
-        return this.totalPrice;
-    }
-    
     equals(shoppingcart: Shoppingcart): boolean {
         return (
             this.id === shoppingcart.getId() &&
-            this.products === shoppingcart.getProducts() &&
+            JSON.stringify(this.cartItems) === JSON.stringify(shoppingcart.getCartItems()) &&
             this.totalPrice === shoppingcart.getTotalPrice()
-        )
+        );
     }
 }
