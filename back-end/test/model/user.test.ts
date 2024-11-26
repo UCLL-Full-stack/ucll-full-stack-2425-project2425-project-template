d1988f2265d7b41ebd6e1b85a5bb178b71bb49e0
Ikki <ikki.vandermeulen@student.ucll.be>
2024-11-26 11:28:13 +0100

    Wrote a test that verifies the event is getting added to user correctly

--- back-end/test/model/user.test.ts
+++ back-end/test/model/user.test.ts
@@ -2,6 +2,7 @@ import { User } from '../../model/user';
 import { Profile } from '../../model/profile';
 import { Location } from '../../model/location';
 import { Category } from '../../model/category';
+import Event from '../../model/event';
 
 const profile = new Profile({
     firstName: 'John',
@@ -29,4 +30,22 @@ test('Given user without a password when making new user then error is thrown', 
     expect(() => {
         const user = new User({ userName: 'TestUser', password: '', profile: profile });
     }).toThrow('Password is required.');
+});
+
+test('Given user and event, when participating to an event, then event gets added to list of event in user', () => {
+    const user = new User({ userName: 'TestUser', password: 'testpasswd', profile: profile });
+    const creationDate = new Date();
+    const date = new Date(3000, 11, 2);
+    const event = new Event({
+        name: 'Pukkelpop',
+        date: date,
+        price: 42.5,
+        minParticipants: 500,
+        maxParticipants: 5000,
+        location: new Location({ street: 'teststreet', number: 5, city: 'test', country: 'test' }),
+        category: new Category({ name: 'test', description: 'description' }),
+    });
+
+    user.addEvent(event);
+    expect(user.getEvents()).toEqual([event]);
 });
