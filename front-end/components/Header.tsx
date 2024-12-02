f04e566cf556231981490599b1fa7848ed77565c
Alan-Kim <r0949915@ucll.be>
2024-12-02 14:05:27 +0100

    removed old initial board creation, added refresh discord, will tweak with the styling later

--- front-end/components/Header.tsx
+++ front-end/components/Header.tsx
@@ -21,6 +21,11 @@ const Header: React.FC<HeaderProps> = ({ onCreateClick, onLoginClick }) => {
         window.location.reload();
     };
 
+    const handleDiscordRefresh = () => {
+        const redirectUri = `http://localhost:8080/api/auth/discord`;
+        window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify guilds`;
+    };        
+
     return (
         <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
             <div className='flex items-center' onClick={handleRefresh}>
@@ -30,38 +35,44 @@ const Header: React.FC<HeaderProps> = ({ onCreateClick, onLoginClick }) => {
             <div>
             {user ? (
                 <div className='relative'>
-                        <div className='flex items-center'>
-                            <button
-                                onClick={onCreateClick}
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            >
-                                Create
-                            </button>
-                            <img
-                                src={user.userAvatar}
-                                alt={`${user.username}'s avatar`}
-                                className="w-10 h-10 rounded-full ml-3"
-                                onClick={() => setDropDownVisible(!dropDownVisible)}
-                            />
-                        </div>
-                        {dropDownVisible && (
-                            <div className="absolute right-0 mt-2 bg-white text-black rounded shadow-lg py-2 z-20">
-                                <button
-                                    onClick={handleLogout}
-                                    className="block px-4 py-2 hover:bg-gray-200 w-full text-left"
-                                >
-                                    Log Out
-                                </button>
-                            </div>
-                        )}
+                    <div className='flex items-center'>
+                        <button
+                            onClick={onCreateClick}
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                            Create
+                        </button>
+                        <img
+                            src={user.userAvatar}
+                            alt={`${user.username}'s avatar`}
+                            className="w-10 h-10 rounded-full ml-3"
+                            onClick={() => setDropDownVisible(!dropDownVisible)}
+                        />
                     </div>
-                ) : (
-                    <button
-                        onClick={onLoginClick}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    >
-                        Login
-                    </button>
+                    {dropDownVisible && (
+                        <div className="absolute right-0 mt-2 bg-white text-black rounded shadow-lg py-2 z-20">
+                            <button
+                                onClick={handleDiscordRefresh}
+                                className="block px-4 py-2 hover:bg-gray-200 w-full text-left"
+                            >
+                                Refresh Discord
+                            </button>
+                            <button
+                                onClick={handleLogout}
+                                className="block px-4 py-2 hover:bg-gray-200 w-full text-left"
+                            >
+                                Log Out
+                            </button>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <button
+                    onClick={onLoginClick}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    Login
+                </button>
                 )}
             </div>
         </header>
