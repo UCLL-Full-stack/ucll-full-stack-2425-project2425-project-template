d4b44679c303842e6b7e284ee3046c62b64f69df
Thomas Bosmans <thomas.bosmans@student.ucll.be>
2024-12-04 23:38:41 +0100

    Working Database with testData

---
+++ back-end/repository/prisma/migrations/20241204223155_fixing_team_seeding_bug_2_0/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX "User_teamId_key";
+
+-- DropIndex
+DROP INDEX "User_userId_teamId_key";
---
+++ back-end/util/seed.ts
@@ -0,0 +1,90 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Empty the database
+  await prisma.match.deleteMany({});
+  await prisma.training.deleteMany({});
+  await prisma.team.deleteMany({});
+  await prisma.user.deleteMany({});
+
+  // Add new users
+  const Chris = await prisma.user.create({
+    data: {
+      firstName: 'Coach',
+      lastName: 'Chris',
+      password: 'coachpassword',
+      role: 'COACH',
+    },
+  });
+
+  const Admin = await prisma.user.create({
+    data: {
+      firstName: 'Admin',
+      lastName: 'Adrie',
+      password: 'adminpassword',
+      role: 'ADMIN',
+    },
+  });
+
+  const John = await prisma.user.create({
+    data: {
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'playerpassword',
+      role: 'PLAYER',
+    },
+  });
+
+  const Jane = await prisma.user.create({
+    data: {
+      firstName: 'Jane',
+      lastName: 'Smith',
+      password: 'playerpassword',
+      role: 'PLAYER',
+    },
+  });
+
+  const Alice = await prisma.user.create({
+    data: {
+      firstName: 'Alice',
+      lastName: 'Johnson',
+      password: 'playerpassword',
+      role: 'PLAYER',
+    },
+  });
+
+  // Create a team with the coach and players
+  const team = await prisma.team.create({
+    data: {
+      coachId: Chris.userId,
+      members: {
+        connect: [
+          { userId: John.userId },
+          { userId: Jane.userId },
+          { userId: Alice.userId },
+        ],
+      },
+    },
+  });
+
+  // Update the coach with the teamId
+  await prisma.user.update({
+    where: { userId: Chris.userId },
+    data: { teamId: team.teamId },
+  });
+}
+
+(async () => {
+  try {
+    await main();
+    await prisma.$disconnect();
+    console.log('Seed successful');
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+    console.log('Seed failed');
+  }
+})();
