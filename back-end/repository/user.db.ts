945cbc3d70a3ff06277dd81e5853e106efdc21f5
Fnafke <elin.bachvarov@student.ucll.be>
2024-12-07 12:05:08 +0100

    changed all functions in userdb to use the prisma database

--- back-end/repository/user.db.ts
+++ back-end/repository/user.db.ts
@@ -1,5 +1,7 @@
 import {User} from "../model/user"
 import { Role } from "../types"
+import database from "../util/database";
+
 
 // empty userlist
 const users: User[] = []
@@ -12,17 +14,48 @@ users.push(new User({name: "Johnjr", email: "johnjr@email.com", password: "VeryS
 //----functions----//
 
 // GET
-const getAllUsers = (): User[] => {
-    return users;
+const getAllUsers = async(): Promise<User[]> => {
+    try {
+        const userPrisma = await database.user.findMany();
+        return userPrisma.map((userPrisma) => User.from(userPrisma));
+    } catch (error) {
+        console.error(error);
+        throw new Error('Database error: Could not fetch all users, check server logs.')
+    }
 }
 
-const getUserByEmail = (email:string): User | undefined => {
-    return users.find((user) => user.getEmail() === email);
+const getUserByEmail = async(email:string): Promise<User | undefined> => {
+    try {
+        const userPrisma = await database.user.findUnique({
+            where: {email}
+        });
+        
+        return userPrisma ? User.from(userPrisma) : undefined;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Database error: Could not fetch user with email, check server logs.')
+
+    }
 }
 
 // Post
-const createUser = (user: User) => {
-    users.push(user);
+const createUser = async(user: User): Promise<User> => {
+    try {
+        const userPrisma = await database.user.create({
+            data: {
+                name: user.getName().toString(),
+                email: user.getEmail().toString(),
+                password: user.getPassword().toString(),
+                role: user.getRole()
+            }
+        });
+
+        return User.from(userPrisma);
+
+    } catch (error) {
+        console.error(error);
+        throw new Error('Database error: Could not create user, check server logs.')
+    }
 }
 
 // export functions
