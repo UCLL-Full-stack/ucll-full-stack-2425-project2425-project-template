57423b948b742d39acd2c632b220b52dd646fa3e
Ikki <ikki.vandermeulen@student.ucll.be>
2024-12-10 09:14:59 +0100

    Edit page

--- front-end/pages/events/edit/[id].tsx
+++ front-end/pages/events/edit/[id].tsx
@@ -2,12 +2,9 @@ import EventService from "@/services/EventService";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Event, User } from "@/types";
-import styles from "@/styles/eventDetails.module.css";
 import Head from "next/head";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { GetServerSideProps } from "next";
 
-const EventDetails: React.FC = () => {
+const EditEventPage: React.FC = () => {
   const router = useRouter();
   const [event, setEvent] = useState<Event | null>(null);
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
@@ -38,8 +35,9 @@ const EventDetails: React.FC = () => {
           <meta name="description" content="Eventer home page" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
+        <>test</>
       </>
     )
   );
 };
-export default EventDetails;
+export default EditEventPage;
