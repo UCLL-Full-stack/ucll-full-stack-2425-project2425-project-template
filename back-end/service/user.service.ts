18c08c16fc4da1f8e8def2567b2f1cc40a2b551c
N4thanJ <nathan.jordens@student.ucll.be>
2024-12-10 20:16:10 +0100

    Service done

--- back-end/service/user.service.ts
+++ back-end/service/user.service.ts
@@ -1,6 +1,10 @@
 import userDb from '../repository/user.db';
 import { User } from '../model/user';
 
+import bcrypt from 'bcrypt';
+import { AuthenticationResponse, UserInput } from '../types';
+import generateSWToken from '../util/jwt';
+
 const getAllUsers = async (): Promise<User[]> => {
     const users = await userDb.getAll();
     if (!users) {
@@ -8,6 +12,51 @@ const getAllUsers = async (): Promise<User[]> => {
     }
 
     return users;
+};
+
+const createUser = async (user: UserInput): Promise<User> => {
+    const existingUser = await userDb.getByEmail({ email: user.email });
+
+    if (existingUser) {
+        throw new Error(`User with email: ${user.email} already exists.`);
+    }
+
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    const newUser = new User({
+        email: user.email,
+        role: user.role,
+        password: hashedPassword,
+    });
+
+    return userDb.createUser(newUser);
+};
+
+const authenticate = async ({
+    email,
+    password,
+}: {
+    email: string;
+    password: string;
+}): Promise<AuthenticationResponse> => {
+    const foundUser = await userDb.getByEmail({ email });
+
+    if (!foundUser) {
+        throw new Error(`User with email: ${email} does not exist.`);
+    }
+
+    await bcrypt.compare(password, foundUser.getPassword());
+
+    const jwt = generateSWToken({ email, role: foundUser.getRole() });
+
+    const authresponse = {
+        message: 'Authentication successful',
+        token: jwt,
+        email: foundUser.getEmail(),
+        role: foundUser.getRole(),
+    };
+
+    return authresponse;
 };
 
 export default { getAllUsers };
