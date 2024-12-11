f40ea5af3ecc15d7e577a11a4bae9245f306c63d
MeesVerbeeck <mees.verbeeck@student.ucll.be>
2024-12-11 22:10:20 +0100

    remove createGroup function

--- front-end/services/groupService.tsx
+++ front-end/services/groupService.tsx
@@ -4,27 +4,6 @@ const getToken = () => {
     return JSON.parse(sessionStorage.getItem('loggedInUser') || '{}').token;
 }
 
-const createGroup = (name: String, description: String, users?: User[]) => {
-    const data = {
-        name: name,
-        description: description,
-        users: users
-    };
-
-    const jsonData = JSON.stringify(data);
-
-    const token = getToken();
-
-    return fetch(`${process.env.NEXT_PUBLIC_API_URL}/groups`, {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-        },
-        body: jsonData
-    });
-};
-
 const getGroups = () => {
     const token = getToken();
 
@@ -37,6 +16,5 @@ const getGroups = () => {
 };
 
 export default {
-    createGroup,
     getGroups,
 };
