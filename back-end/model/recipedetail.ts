2c5986ffabf25930e9af5e0f97fffe9c37486e2b
thomasgorisucll <79001636+thomasgorisucll@users.noreply.github.com>
2024-12-12 11:31:47 +0100

    added the gettters and the equals methode

--- back-end/model/recipedetail.ts
+++ back-end/model/recipedetail.ts
@@ -4,11 +4,40 @@ export class RecipeDetail {
     private difficulty: number;
     private cookingTime: number;
 
-    constructor(recipeDetail: { recipeDetailId?: number, totalLikes: number, difficulty: number, cookingTime: number }) {
+    constructor(recipeDetail: {
+        recipeDetailId?: number,
+        totalLikes: number,
+        difficulty: number,
+        cookingTime: number
+    }) {
         this.recipeDetailId = recipeDetail.recipeDetailId;
         this.totalLikes = recipeDetail.totalLikes;
         this.difficulty = recipeDetail.difficulty;
         this.cookingTime = recipeDetail.cookingTime;
+    }
+
+    getRecipeDetailId(): number | undefined {
+        return this.recipeDetailId;
+    }
+
+    getTotalLikes(): number {
+        return this.totalLikes;
+    }
+
+    getDifficulty(): number {
+        return this.difficulty;
+    }
+
+    getCookingTime(): number {
+        return this.cookingTime;
+    }
+
+    equals(recipeDetail: RecipeDetail): boolean {
+        return (
+            this.totalLikes === recipeDetail.getTotalLikes() &&
+            this.difficulty === recipeDetail.getDifficulty() &&
+            this.cookingTime === recipeDetail.getCookingTime()
+        )
     }
 
     public addLike(): void {
