5d6d3403f5c900ff2575763692620a959a792a5c
Ashley Timmermans <ashley.timmermans@student.ucll.be>
2024-12-15 18:44:54 +0100

    Vertaling voor home page aangemaakt in engels en nederlands. hiervoor een dropdown menu toegevoegd met stijling

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,21 +1,37 @@
 import Head from 'next/head';
 import Header from '@/components/header';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const Home: React.FC = () => {
+
+  const { t } = useTranslation();
+
   return (
     <>
       <Head>
-        <title>BowlBuddies</title>
+        <title>{t("app.title")}</title>
         <meta name="description" content="BowlBuddies Pokebowl app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="assets/logo.png" />
       </Head>
       <Header />
       <main>
-        <h1>BowlBuddies Pokebowls</h1>
+        <h1>{t("page.title")}</h1>
       </main>
     </>
   );
 };
+
+export const getServerSideProps = async (context: { locale: any; }) => {
+  const { locale } = context;
+
+  return {
+      props: {
+          ...(await serverSideTranslations(locale ?? "en", ["common"])),
+      },
+  };
+};
+
 
 export default Home;
