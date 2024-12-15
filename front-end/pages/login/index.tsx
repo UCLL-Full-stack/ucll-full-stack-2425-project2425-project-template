f18dc700f419c3b006aa35b2dcda741ce89d405c
Ashley Timmermans <ashley.timmermans@student.ucll.be>
2024-12-15 18:55:25 +0100

    Vertaling van header op alle pagina's gefixt

--- front-end/pages/login/index.tsx
+++ front-end/pages/login/index.tsx
@@ -1,8 +1,13 @@
 import Header from "@/components/header";
 import LoginForm from "@/components/users/LoginForm";
 import Head from "next/head";
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
 
 const Login: React.FC = () => {
+    const { t } = useTranslation();
+
     return (
         <>
             <Head>
@@ -20,5 +25,16 @@ const Login: React.FC = () => {
         </>
     );
 };
+
+export const getServerSideProps = async (context: { locale: any; }) => {
+    const { locale } = context;
+  
+    return {
+        props: {
+            ...(await serverSideTranslations(locale ?? "en", ["common"])),
+        },
+    };
+};
+  
 
 export default Login;
