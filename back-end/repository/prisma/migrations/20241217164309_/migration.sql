802756ad83206d1042ae341dfe9757a29c0aa020
VincentVanbrabantUCLL <r0900502@ucll.be>
2024-12-17 18:02:01 +0100

    seed updated and migrtations gone

--- back-end/repository/prisma/migrations/20241217164309_/migration.sql
+++ back-end/repository/prisma/migrations/20241217164309_/migration.sql
@@ -33,6 +33,7 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
