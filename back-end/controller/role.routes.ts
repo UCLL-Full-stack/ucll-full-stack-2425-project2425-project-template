00b2d8707ab20ce4fa8a4695082871b4db46dcdd
Eddy Ndacasaba <eddyndacasaba@student.ucll.be>
2024-12-18 18:04:23 +0100

    Swagger Updated

--- back-end/controller/role.routes.ts
+++ back-end/controller/role.routes.ts
@@ -2,9 +2,55 @@ import { Router } from "express";
 import roleService from "../service/role.service";
 import { CreateRoleInput } from "../types";
 
-
 const roleRouter = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Role:
+ *       type: object
+ *       properties:
+ *         roleId:
+ *           type: string
+ *           description: Unique identifier for the role
+ *         roleName:
+ *           type: string
+ *           description: Name of the role
+ *         permissions:
+ *           type: array
+ *           items:
+ *             type: string
+ *             description: List of permissions assigned to the role
+ *         guildId:
+ *           type: string
+ *           description: Guild to which the role belongs
+ * 
+ *     ErrorResponse:
+ *       type: object
+ *       properties:
+ *         error:
+ *           type: string
+ *           description: Error message
+ */
+
+/**
+ * @swagger
+ * /api/roles:
+ *   get:
+ *     summary: Retrieve a list of roles
+ *     responses:
+ *       200:
+ *         description: A list of roles
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Role'
+ *       404:
+ *         description: No roles found
+ */
 roleRouter.get("/", async (req, res) => {
     try {
         const roles = await roleService.getAllRoles();
@@ -18,6 +64,28 @@ roleRouter.get("/", async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/roles/{roleId}:
+ *   get:
+ *     summary: Retrieve a specific role by ID
+ *     parameters:
+ *       - in: path
+ *         name: roleId
+ *         required: true
+ *         description: The ID of the role to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Role retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Role'
+ *       404:
+ *         description: Role not found
+ */
 roleRouter.get("/:roleId", async (req, res) => {
     const { roleId } = req.params;
     try {
@@ -32,6 +100,23 @@ roleRouter.get("/:roleId", async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/roles:
+ *   post:
+ *     summary: Create a new role
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Role'
+ *     responses:
+ *       201:
+ *         description: Role created successfully
+ *       400:
+ *         description: Bad request
+ */
 roleRouter.post("/", async (req, res) => {
     const { roleId, roleName, permissions = [], guildId } = req.body;
     const createRole: CreateRoleInput = { roleId, roleName, permissions, guildId };
@@ -47,6 +132,30 @@ roleRouter.post("/", async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/roles/{roleId}:
+ *   put:
+ *     summary: Update a role
+ *     parameters:
+ *       - in: path
+ *         name: roleId
+ *         required: true
+ *         description: The ID of the role to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Role'
+ *     responses:
+ *       200:
+ *         description: Role updated successfully
+ *       400:
+ *         description: Bad request
+ */
 roleRouter.put("/:roleId", async (req, res) => {
     const { roleId } = req.params;
     const { roleName, permissions } = req.body;
