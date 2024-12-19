6735f874188a5f480e8b95c93c781601e98f5152
r0887562 <aaron.abbey@student.ucll.be>
2024-12-19 02:30:29 +0100

    authentication werkt

--- back-end/app.ts
+++ back-end/app.ts
@@ -16,14 +16,14 @@ const app = express();
 dotenv.config();
 const port = process.env.APP_PORT || 3000;
 
-// app.use(
-//     expressjwt({
-//         secret: process.env.JWT_SECRET || 'default_secret',
-//         algorithms: ['HS256'],
-//     }).unless({
-//         path: ['/api-docs', /^\/api-docs\/.*/, '/users/login', '/users/signup', '/status' ]
-//     })
-// )
+app.use(
+    expressjwt({
+        secret: process.env.JWT_SECRET || 'default_secret',
+        algorithms: ['HS256'],
+    }).unless({
+        path: ['/api-docs', /^\/api-docs\/.*/, '/users/login', '/users/signup', '/status' ]
+    })
+)
 
 app.use(cors());
 app.use(bodyParser.json());
