e8509e3817a27b2a1496ecf734b7d1075c5c1103
Cedric Van Uytsel <cedricvu@hotmail.be>
2024-12-19 11:19:47 +0100

    world selection part 1

--- front-end/components/game/WorldSelection.tsx
+++ front-end/components/game/WorldSelection.tsx
@@ -1,35 +1,45 @@
 import userService from '@services/userService';
 import playerService from '@services/playerService';
 import { useState, useEffect } from 'react';
-import { Player, User } from '@types';
+import { Player, User, World } from '@types';
+import worldService from '@services/worldService';
 
 const WorldSelection: React.FC = () => {
     const [loggedInUser, setLoggedInUser] = useState<User>();
-    const [characters, setCharacters] = useState<Player[]>();
-    const [selectedCharacter, setSelectedCharacter] = useState<Player>();
-    const [detectChange, setDetect] = useState<boolean>();
+    const [userWorlds, setUserWorlds] = useState<World[]>();
+    const [allWorlds, setAllWorlds] = useState<World[]>();
 
-    const getCharacters = async () => {
+    const getWorlds = async () => {
         if (loggedInUser){
-            setCharacters(await playerService.getPlayersFromUser(loggedInUser.email));
+            const worlds = await worldService.getWorlds();
+            setAllWorlds(worlds);
         }
     }
 
-    const getSelectedCharacter = async () => {
-        const select = localStorage.getItem('selectedCharacter');
-        if (select) {
-            setSelectedCharacter(await playerService.getPlayerById(select));
+    const getUserWorlds = async () => {
+        if (allWorlds && loggedInUser){
+            let result: World[] = [];
+            allWorlds.forEach(world => {
+                if (world.owner.email === loggedInUser.email) {
+                    result.push(world);
+                }
+            })
+            setUserWorlds(result);
         }
     }
 
-    const changeSelectedCharacter = async (character: Player) => {
-        localStorage.setItem("selectedCharacter", character.id.toString());
-        setDetect(!detectChange);
+    const joinworld = async (id: number) => {
+        console.log("hallo");
     }
 
     useEffect(() => {
-        getCharacters();
+        getWorlds();
     }, [loggedInUser]);
+
+    
+    useEffect(() => {
+        getUserWorlds();
+    }, [allWorlds]);
 
     useEffect(() => {
         const user = localStorage.getItem('loggedInUser');
@@ -38,63 +48,28 @@ const WorldSelection: React.FC = () => {
         }
     }, []);
 
-    useEffect(() => {
-        getSelectedCharacter();
-    }, [detectChange]);
-
-    if (!characters){
-        return <p>Loading Characters...</p>
+    if (!allWorlds){
+        return <p>Loading Worlds...</p>
     }
 
-    if (characters.length <= 0){
-        return <p>Please make a player character first</p>
+    if (!userWorlds){
+        return <p>Loading Worlds...</p>
     }
 
     return (
         <div className="flex flex-col items-center justify-center ">
-            <table className="min-w-full">
-                <thead className="bg-gray-50">
-                    <tr>
-                        <th scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Character
-                        </th>
-                        <th scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Money
-                        </th>
-                        <th scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Statistics
-                        </th>
-                        <th scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Class
-                        </th>
-                    </tr>
-                </thead>
-                <tbody className="bg-white">
-                {characters && characters.length > 0 && characters.map((character, index) => (    
+            {userWorlds.length <= 0 ? (
+                <p>You have no worlds</p>
+            ):(
                 <>
-                    {character.id === selectedCharacter?.id ? (
-                        <tr className="" key={index} onClick={() => changeSelectedCharacter(character)}>
-                            <td className="px-6 py-4 whitespace-nowrap">• {character.name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{character.currency}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{character.statistics}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{character.class}</td>
-                        </tr>
-                    ):(
-                        <tr key={index} onClick={() => changeSelectedCharacter(character)}>
-                            <td className="px-6 py-4 whitespace-nowrap">{character.name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{character.currency}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{character.statistics}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{character.class}</td>
-                        </tr>
-                    )}
-                </>
+                <p className='text-2xl m-6'>Your Worlds</p>
+                {userWorlds.map((world, index) => (
+                    <div className='border-solid hover:border-dotted border-2 rounded p-12' key={index} onClick={() => joinworld(world.id)}>
+                        <p className='text-2xl'>{world.name}</p>
+                    </div>
                 ))}
-                </tbody>
-            </table>
+                </>
+            )}
         </div>
     );
 };
--- front-end/services/worldService.ts
+++ front-end/services/worldService.ts
@@ -9,6 +9,17 @@ const getWorldById = async (id: string) => {
     return await res.json();
 };
 
+const getWorlds = async () => {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_URL + '/world/', {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+        },
+    });
+    return await res.json();
+};
+
 export default {
     getWorldById,
+    getWorlds,
 };
