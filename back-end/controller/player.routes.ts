b7d5a41cde5ab9c9915ae6c40f340b8ddc5c9dd7
XanderDhondtUCLL <xander.dhondt@live.be>
2024-12-20 20:00:50 +0100

    added swagger documentation

--- back-end/controller/player.routes.ts
+++ back-end/controller/player.routes.ts
@@ -4,9 +4,47 @@ import { PlayerInput } from '../types';
 
 const playerRouter = express.Router();
 
-/*
-swagger documentation to be added.
-*/
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   schemas:
+ *     Player:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         name:
+ *           type: string
+ *         imageUrl:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * /players:
+ *   get:
+ *     summary: Retrieve a list of players
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of players.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Player'
+ *       404:
+ *         description: Player not found.
+ *       500:
+ *         description: Internal server error.
+ */
 playerRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const players = await playerService.getAllPlayers();
@@ -16,10 +54,33 @@ playerRouter.get('/', async (req: Request, res: Response, next: NextFunction) =>
     }
 });
 
-/*
-swagger documentation to be added.
-*/
-
+/**
+ * @swagger
+ * /players/{id}:
+ *   get:
+ *     summary: Retrieve a player by ID
+ *     description: Fetches the player specified by the ID parameter.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the player to retrieve.
+ *         schema:
+ *           type: integer
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A player object.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Player'
+ *       404:
+ *         description: Player not found.
+ *       500:
+ *         description: Internal server error.
+ */
 playerRouter.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const player = await playerService.getPlayerById(Number(req.params.id));
@@ -32,10 +93,37 @@ playerRouter.get('/:id', async (req: Request, res: Response, next: NextFunction)
     }
 });
 
-/*
-swagger documentation to be added.
-*/
-
+/**
+ * @swagger
+ * /players/image/{id}:
+ *   get:
+ *     summary: Retrieve the image of a player by ID
+ *     description: Fetches the image associated with the player specified by the ID parameter.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the player to retrieve the image for.
+ *         schema:
+ *           type: integer
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A player image object.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 imageUrl:
+ *                   type: string
+ *                   description: URL of the player's image.
+ *       404:
+ *         description: Player not found.
+ *       500:
+ *         description: Internal server error.
+ */
 playerRouter.get('/image/:id', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const player = await playerService.getPlayerImage(Number(req.params.id));
@@ -48,10 +136,35 @@ playerRouter.get('/image/:id', async (req: Request, res: Response, next: NextFun
     }
 });
 
-/*
-swagger documentation to be added.
-*/
-
+/**
+ * @swagger
+ * /players/user/{email}:
+ *   get:
+ *     summary: Retrieve players by user email
+ *     description: Fetches the players associated with the user specified by the email parameter.
+ *     parameters:
+ *       - in: path
+ *         name: email
+ *         required: true
+ *         description: Email of the user to retrieve players for.
+ *         schema:
+ *           type: string
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of players.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Player'
+ *       404:
+ *         description: User not found.
+ *       500:
+ *         description: Internal server error.
+ */
 playerRouter.get('/user/:email', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const players = await playerService.getPlayersByUser(req.params.email);
@@ -59,19 +172,41 @@ playerRouter.get('/user/:email', async (req: Request, res: Response, next: NextF
     } catch (error) {
         res.status(400).json({
             status: '400',
-            errorMessage: `User with id ${req.params.id} does not exist.`,
+            errorMessage: `User with email ${req.params.email} does not exist.`,
         });
     }
 });
 
-/*
-swagger documentation to be added.
-*/
-
+/**
+ * @swagger
+ * /players/add:
+ *   post:
+ *     summary: Add a new player
+ *     description: Creates a new player with the provided data.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Player'
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: The created player object.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Player'
+ *       400:
+ *         description: Bad request.
+ *       500:
+ *         description: Internal server error.
+ */
 playerRouter.post('/add', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const player = <PlayerInput>req.body;
-        const result = await playerService.addPlayer(player)
+        const result = await playerService.addPlayer(player);
         res.status(200).json(result);
     } catch (error) {
         res.status(400).json({
@@ -79,6 +214,6 @@ playerRouter.post('/add', async (req: Request, res: Response, next: NextFunction
             errorMessage: `Something went wrong with creating player.`,
         });
     }
-})
+});
 
 export { playerRouter };
